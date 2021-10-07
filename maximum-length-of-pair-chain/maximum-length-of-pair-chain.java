97b858a6028299e9aca2ca5963120be79338b0b7
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-07 14:10:43 +0530

    Time: 8 ms (97.81%), Space: 39.3 MB (74.00%) - LeetHub

--- maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,12 +1,24 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
-        int cur = Integer.MIN_VALUE, ans = 0;
-        for (int[] pair: pairs) if (cur < pair[0]) {
-            cur = pair[1];
-            ans++;
-        }
+        Arrays.sort(pairs,(a,b) -> a[1] - b[1]);
+        int curr=Integer.MIN_VALUE;
+        int ans=0;
+        for(int pair[]: pairs )
+            if(curr<pair[0])
+            {
+                curr=pair[1];
+                ans++;
+            }
         return ans;
-  
     }
 }
+        
+        
+        
+        
+        
+        
+    
+/*Time Complexity: O(NlogN) where Nis the length of S. The complexity comes from the sorting step, but the rest of the solution does linear work.
+
+Space Complexity: O(N). The additional space complexity of storing cur and ans, but sorting uses O(N) space. Depending on the implementation of the language used, sorting can sometimes use less space.*/
