c9ea387c922c8b3d1081bb25be091c2960e47802
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-09 23:17:01 +0530

    Time: 3 ms (82.44%), Space: 39.7 MB (47.13%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -1,22 +1,68 @@
 class Solution {
-     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        getResult(result, new ArrayList<Integer>(), candidates, target, 0);
-        
-        return result;
+ protected void backtrack(
+            int remain,
+            LinkedList<Integer> comb,
+            int start, int[] candidates,
+            List<List<Integer>> results) {
+
+        if (remain == 0) {
+            // make a deep copy of the current combination
+            results.add(new ArrayList<Integer>(comb));
+            return;
+        } else if (remain < 0) {
+            // exceed the scope, stop exploration.
+            return;
+        }
+
+        for (int i = start; i < candidates.length; ++i) {
+            // add the number into the combination
+            comb.add(candidates[i]);
+            this.backtrack(remain - candidates[i], comb, i, candidates, results);
+            // backtrack, remove the number from the combination
+            comb.removeLast();
+        }
     }
-    
-    private void getResult(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start){
-    	if(target > 0){
-    		for(int i = start; i < candidates.length && target >= candidates[i]; i++){
-    			cur.add(candidates[i]);
-    			getResult(result, cur, candidates, target - candidates[i], i);
-    			cur.remove(cur.size() - 1);
-    		}//for
-    	}//if
-    	else if(target == 0 ){
-    		result.add(new ArrayList<Integer>(cur));
-    	}//else if
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        LinkedList<Integer> comb = new LinkedList<Integer>();
+
+        this.backtrack(target, comb, 0, candidates, results);
+        return results;
     }
 }
+/*Complexity Analysis
+
+Let NN be the number of candidates, TT be the target value, and MM be the minimal value among the candidates.
+
+Time Complexity: \mathcal{O}(N^{\frac{T}{M}+1})
+ )
+
+As we illustrated before, the execution of the backtracking is unfolded as a DFS traversal in a n-ary tree. The total number of steps during the backtracking would be the number of nodes in the tree.
+
+At each node, it takes a constant time to process, except the leaf nodes which could take a linear time to make a copy of combination. So we can say that the time complexity is linear to the number of nodes of the execution tree.
+
+Here we provide a loose upper bound on the number of nodes.
+
+First of all, the fan-out of each node would be bounded to NN, i.e. the total number of candidates.
+
+The maximal depth of the tree, would be \frac{T}{M}, where we keep on adding the smallest element to the combination.
+
+As we know, the maximal number of nodes in N-ary tree of \frac{T}{M} 
+
+  height would be N^{\frac{T}{M}+1}
+
+Note that, the actual number of nodes in the execution tree would be much smaller than the upper bound, since the fan-out of the nodes are decreasing level by level.
+
+Space Complexity: \mathcal{O}(\frac{T}{M})
+We implement the algorithm in recursion, which consumes some additional memory in the function call stack.
+
+The number of recursive calls can pile up to \frac{T}{M} 
+
+ , where we keep on adding the smallest element to the combination. As a result, the space overhead of the recursion is \mathcal{O}(\frac{T}{M})
+
+In addition, we keep a combination of numbers during the execution, which requires at most \mathcal{O}(\frac{T}{M}) space as well.
+
+To sum up, the total space complexity of the algorithm would be \mathcal{O}(\frac{T}{M})
+
+Note that, we did not take into the account the space used to hold the final results for the space complexity*/
