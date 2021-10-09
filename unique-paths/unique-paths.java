fbf7409c525546024c0d15063234cbe1bdd7f14b
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-09 19:24:11 +0530

    Time: 0 ms (100.00%), Space: 37.5 MB (36.78%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,16 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-             int N = (m-1) + (n-1); // no of steps in each valid path
-        int r = m-1;
-        /*
-        N C m-1 OR
-        N C n-1 would give number of such valid unique paths
-        */
-        double res = 1;
-        for(int i=1; i<=r; i++){
-            res = res* (N - r + i)/i; 
-        }
-        
-        return (int) res;
+         int[][] d = new int[m][n];
+
+    for(int[] arr : d) {
+      Arrays.fill(arr, 1);
+    }
+    for(int col = 1; col < m; ++col) {
+      for(int row = 1; row < n; ++row) {
+        d[col][row] = d[col - 1][row] + d[col][row - 1];
+      }
+    }
+    return d[m - 1][n - 1];
     }
 }
