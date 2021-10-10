baaccbd9113199aa16f1e7998197dc18fdbf3041
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-10 23:22:29 +0530

    Time: 0 ms (100.00%), Space: 39 MB (64.88%) - LeetHub

---
+++ number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+            int count=0;
+        for(String x : patterns) if(word.contains(x)) count++;
+        return count;
+    }
+}
