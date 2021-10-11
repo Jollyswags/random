584cb519335cfa952c2028bd54a05b555bebc5e3
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-11 21:34:53 +0530

    Time: 13 ms (30.68%), Space: 38.7 MB (52.45%) - LeetHub

---
+++ count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countSubstrings(String s, String t) {
+        int i,j;
+        int ans=0;
+        int x,y,d;
+        for(i=0;i<s.length();i++)
+        {
+            for(j=0;j<t.length();j++)
+            {
+                d=0;
+                x=i;
+                y=j;
+                while(x<s.length() && y<t.length())
+                {
+                    if(s.charAt(x)!=t.charAt(y))
+                    {
+                        d++;
+                    }
+                    if(d==1)
+                        ans++;
+                    if(d==2)
+                        break;
+                    x++;
+                    y++;
+                }
+            }
+        }
+        return ans;
+    }
+}
