889234ac8fe2cf835c4b573f4f6c47e53ba9c51f
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-22 23:18:12 +0530

    Time: 4 ms (7.82%), Space: 38.1 MB (42.08%) - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> getRow(int n) {
+          List<List<Integer>> row=new ArrayList<>();
+        List<Integer> col;
+        int i,j;
+        for(i=0;i<=n;i++)          
+        {
+            col=new ArrayList<>();
+            if(i==0)
+            {
+                col.add(1);
+                row.add(col);
+                continue;
+            }
+            
+            for(j=0;j<=i;j++)
+            {
+                if(j==0)
+                    col.add(1);
+                else
+                    if(j==i)
+                    {
+                        col.add(1);
+                        row.add(col);
+                        break;
+                    }
+                else
+                {
+                    List<Integer> a=row.get(i-1);
+                    int x=a.get(j-1);
+                    int y=a.get(j);
+                    col.add(x+y);
+                }
+              
+            }
+        }
+        return row.get(n);
+    }
+}
