fbc944f4b934f11b3bf91b558f2c737ed9395ad0
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-10-30 02:10:49 +0530

    Time: 13 ms (94.03%), Space: 39.9 MB (56.36%) - LeetHub

--- minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,23 +1,32 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-          int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            dp[i][s2.length()] = dp[i+1][s2.length()] + s1.codePointAt(i);
-        }
-        for (int j = s2.length() - 1; j >= 0; j--) {
-            dp[s1.length()][j] = dp[s1.length()][j+1] + s2.codePointAt(j);
-        }
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            for (int j = s2.length() - 1; j >= 0; j--) {
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = dp[i+1][j+1];
-                } else {
-                    dp[i][j] = Math.min(dp[i+1][j] + s1.codePointAt(i),
-                                        dp[i][j+1] + s2.codePointAt(j));
-                }
+        int i,j;
+        int m=s1.length();
+        int n=s2.length();
+        int lcs[][]=new int[m+1][n+1];
+        for(i=0;i<=m;i++)
+        {
+            for(j=0;j<=n;j++)
+            {
+                if(i==0 || j==0)
+                    lcs[i][j]=0;
+                else
+                    if(s1.charAt(i-1)==s2.charAt(j-1))
+                        lcs[i][j]=lcs[i-1][j-1]+s1.codePointAt(i-1)+s2.codePointAt(j-1);
+                else
+                    lcs[i][j]=Math.max(lcs[i-1][j],lcs[i][j-1]);
             }
         }
-        return dp[0][0];
+        int l=0;
+        for(i=0;i<m;i++)
+        {
+            l+=s1.codePointAt(i);
+        }
+        for(j=0;j<n;j++)
+        {
+            l+=s2.codePointAt(j);
+        }
+        return l-lcs[m][n];
+        
     }
 }
