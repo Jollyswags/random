232d0d18ae22ad1da523744b949b17c1a7bc06e4
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-11-02 23:20:03 +0530

    Time: 11 ms (96.31%), Space: 38.2 MB (92.73%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,6 +1,6 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-           int max = amount + 1;
+        int max = amount + 1;
     int[] dp = new int[amount + 1];
     Arrays.fill(dp, max);
     dp[0] = 0;
@@ -14,5 +14,3 @@ class Solution {
     return dp[amount] > amount ? -1 : dp[amount];
     }
 }
-/*ime complexity : O(S∗n). On each step the algorithm finds the next F(i)F(i) in nn iterations, where 1≤i≤S. Therefore in total the iterations are S∗n.
-Space complexity : O(S). We use extra space for the memoization table.*/
