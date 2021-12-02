4285974e7a9117b5930d7f278cecf3b375f1030f
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-12-03 01:55:15 +0530

    Time: 6 ms (99.64%), Space: 39.6 MB (86.12%) - LeetHub

---
+++ design-parking-system/design-parking-system.java
@@ -0,0 +1,23 @@
+class ParkingSystem {
+    public int parkingspace[]=new int[3];
+
+    public ParkingSystem(int big, int medium, int small) {
+        parkingspace[0]=big;
+        parkingspace[1]=medium;
+        parkingspace[2]=small;
+        
+    }
+    
+    public boolean addCar(int carType) {
+        if(parkingspace[carType-1]<=0)
+            return false;
+        parkingspace[carType-1]--;
+        return true;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
