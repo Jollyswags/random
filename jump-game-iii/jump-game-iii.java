b11ab74ac193adcd8fe9e6b038e03b4a8e436022
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-12-09 13:38:12 +0530

    Time: 2 ms (85.86%), Space: 52.3 MB (40.80%) - LeetHub

--- jump-game-iii/jump-game-iii.java
+++ jump-game-iii/jump-game-iii.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        boolean visited[]=new boolean[arr.length];
+        int n=arr.length;
+        boolean visited[]=new boolean[n];
         return dfs(arr,start,visited);
         
     }
-    public boolean dfs(int arr[],int start,boolean visited[])
+    public boolean  dfs(int arr[], int start, boolean visited[])
     {
-        if(start<0 || start>arr.length-1 || visited[start])
+        if(start<0 || start>=arr.length || visited[start])
+        {
             return false;
+        }
         if(arr[start]==0)
             return true;
         visited[start]=true;
-        return dfs(arr,start+arr[start],visited) || dfs(arr,start-arr[start],visited);
+        return dfs(arr,start+arr[start], visited) || dfs(arr,start-arr[start], visited);
     }
 }
