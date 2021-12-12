de8def1a40640a0058c3b545f07adde5c76cc59c
Jolly <87942676+Jollyswags@users.noreply.github.com>
2021-12-12 11:12:44 +0530

    Time: 1 ms (99.86%), Space: 43.2 MB (71.33%) - LeetHub

--- counting-bits/counting-bits.java
+++ counting-bits/counting-bits.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] countBits(int n) {
-        int arr[]=new int[n+1];
+        int bits[]=new int[n+1];
+       
         int i;
         for(i=0;i<=n;i++)
         {
             if(i%2==0)
-                arr[i]=arr[i/2];
+                bits[i]=bits[i/2];
             else
-                arr[i]=arr[i/2]+1;
+                bits[i]=bits[i/2]+1;
         }
-        return arr;
-        
+        return bits;
     }
 }
-//tc=o(n)
-//sc=o(1)
