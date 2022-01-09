b10c4402087cdb1f353bfed15ab438d89774228b
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-09 22:01:10 +0530

    Time: 1 ms (47.90%), Space: 48.8 MB (29.34%) - LeetHub

---
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
+        return sufficientSubset(root, limit, 0);
+    }
+     public TreeNode sufficientSubset(TreeNode root, int limit, int sum) {
+    if (root == null) {
+      return null;
+    }
+    sum = sum + root.val;
+    
+    if (root.left == null && root.right == null) {
+      if (sum < limit) {
+        return null;
+      } else {
+        return root;
+      }
+    }
+    
+    root.left = sufficientSubset(root.left, limit, sum);
+    root.right = sufficientSubset(root.right, limit, sum);
+    
+    if (root.left == null && root.right == null) {
+      return null;
+    }
+    return root;
+     }
+}
