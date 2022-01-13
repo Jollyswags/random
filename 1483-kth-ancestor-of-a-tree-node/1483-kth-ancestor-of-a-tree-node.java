6c80637fc7e16faa795d402dc00d6c3f0cdd376a
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-13 17:58:16 +0530

    Time: 91 ms (54.41%), Space: 113.3 MB (36.03%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
@@ -1,30 +1,33 @@
 class TreeAncestor {
-
- int maxDepth;
-    int [][] tree;
+int tree[][];
+    int maxDepth;
     public TreeAncestor(int n, int[] parent) {
-        maxDepth = (int) (Math.log(n) / Math.log(2)) + 1;
-        tree = new int [maxDepth][n];
-        tree[0] = parent;
-        for (int i = 1; i < maxDepth; i++) {
-            for (int j = 0; j < n; j++) {
-                int pre = tree[i - 1][j];
-                tree[i][j] = pre == -1 ? -1 : tree[i - 1][pre]; 
+        int pre;
+        maxDepth=(int)(Math.log(n)/Math.log(2)) +1;
+        tree=new int[maxDepth][n];
+        tree[0]=parent;
+        int i,j;
+        for(i=1;i<maxDepth;i++)
+            for(j=0;j<n;j++)
+            {
+                pre=tree[i-1][j];
+                tree[i][j]= pre==-1?-1:tree[i-1][pre];
             }
-        }
     }
     
     public int getKthAncestor(int node, int k) {
-        int max = maxDepth;
-        while (k > 0 && node > -1) {
-            int num = 1 << max; // 2^max
-            if(k >= 1 << max) {
-                node = tree[max][node];
-                k -= 1 << max;
+        int max=maxDepth;
+        int num;
+        while(k>0 && node>-1)
+        {
+            num=1<<max;
+            if(k>=1<<max)
+            {
+                node=tree[max][node];
+                k-=1<<max;
             }
-            else {
-                max -= 1;
-            }
+            else
+                max-=1;
         }
         return node;
     }
