35c70471c8afd4d1ae95669e2f8cf9cb04b0ceea
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-20 14:28:23 +0530

    Time: 3 ms (53.22%), Space: 45.4 MB (42.46%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,35 +1,60 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Arrays;
+/**
+ * Using Binary Search + PriorityQueue (Max Heap)
+ *
+ * Find the left most zero in each row (using binary search). Save the row index
+ * and count combination in max heap of size k.
+ *
+ * Time Complexity:
+ * O(M * logN) --> To find the left most zero in each row
+ * O(M * logK) --> To add and poll from PriorityQueue. K elements are maintained in the queue.
+ * Total Time Complexity: O(M * log(NK))
+ *
+ * Space Complexity: O(K) --> Used by MaxHeap
+ *
+ * M = Number of rows. N = Number of Columns.
+ */
 class Solution {
-    public int[] kWeakestRows(int[][] nums, int k) {
-        int m=nums.length;
-        int n=nums[0].length;
-        int i,j,count;
-        for(i=0;i<m;i++)
-        {
-            count=0;
-            for(j=0;j<n;j++)
-            {
-                if(nums[i][j]==0)
-                    break;
-                count++;
+    public int[] kWeakestRows(int[][] mat, int k) {
+        if (mat == null || k < 0 || mat.length < k) {
+            throw new IllegalArgumentException("Input is invalid");
+        }
+
+        // Max Heap
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> (a[1] != b[1] ? b[1] - a[1] : b[0] - a[0]));
+
+        for (int i = 0; i < mat.length; i++) {
+            queue.offer(new int[] { i, getSoldierCount(mat[i]) });
+            if (queue.size() > k) {
+                queue.poll();
             }
-            nums[i][0]=count;
         }
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> (a[1] != b[1] ? b[1] - a[1] : b[0] - a[0]));
-        for(i=0;i<m;i++)
-        {
-            pq.offer(new int[]{i,nums[i][0]});
-             if (pq.size() > k) {
-                pq.poll();
-             }
+
+        int[] result = new int[k];
+        while (!queue.isEmpty()) {
+            result[--k] = queue.poll()[0];
         }
-         int result[]=new int[k];
-            while(!pq.isEmpty())
-            {
-                result[--k]=pq.poll()[0];
+        return result;
+    }
+
+    private int getSoldierCount(int[] row) {
+        if (row[0] == 0) {
+            return 0;
+        }
+        if (row[row.length - 1] == 1) {
+            return row.length;
+        }
+        int start = 0;
+        int end = row.length; // Number of solder can be maximum row.length. Thus end needs to be row.length
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (row[mid] == 1) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
-            return result;      
         }
+
+        return end;
+    }
 }
