8bb3890399c4c6c21a189769d9526756183a2d32
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-21 19:43:39 +0530

    Time: 2 ms (44.97%), Space: 38.3 MB (23.37%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    class CharCount {
+        char c;
+        int count;
+        CharCount(int count, char c) {
+            this.c = c;
+            this.count = count;
+        }
+    }
+    
+    public String longestDiverseString(int ax, int bx, int cx) {
+        PriorityQueue<CharCount> pq =
+            new PriorityQueue<>((a,b)->(b.count-a.count) );
+        if (ax != 0) pq.add(new  CharCount(ax, 'a'));
+        if (bx != 0) pq.add(new  CharCount(bx, 'b'));
+        if (cx != 0) pq.add(new  CharCount(cx, 'c'));
+        
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (!pq.isEmpty()) {
+            CharCount cc = pq.poll();
+            //if not happy, add next max char
+            if (i >= 2 && cc.c == sb.charAt(i-1) && cc.c == sb.charAt(i-2)) {
+                if (pq.isEmpty()) break;
+                //remove and re-add for correct ordering
+                CharCount c2 = pq.poll();
+                c2.count--;
+                if (c2.count > 0) pq.add(c2);
+                
+                sb.append(c2.c);
+                i++;
+            }
+            sb.append(cc.c);
+            cc.count--;
+            if (cc.count > 0) pq.add(cc);
+            i++;
+        }
+        
+        return sb.toString();
+    }
+}
