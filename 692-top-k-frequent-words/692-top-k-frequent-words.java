2976d2ea0db3f6869f4331b967e0c96462cf014a
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-23 21:43:09 +0530

    Time: 7 ms (52.20%), Space: 42.2 MB (27.18%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -7,7 +7,7 @@ class Solution {
         PriorityQueue <Map.Entry<String, Integer>> q
             = new PriorityQueue<>(
                 (a,b) -> a.getValue() != b.getValue() 
-                        ? -1 * a.getValue().compareTo(b.getValue())     // get max frequency at top 
+                        ?  b.getValue()-a.getValue()     // get max frequency at top 
                         : a.getKey().compareTo(b.getKey())              // if frequency same get the lexical order
         );
         for (Map.Entry <String, Integer> m : map.entrySet())
