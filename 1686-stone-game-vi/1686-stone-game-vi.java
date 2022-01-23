fb81e624bb5cd1fa34b838bad887b44b2d6f9b68
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-23 22:03:15 +0530

    Time: 378 ms (6.17%), Space: 114.1 MB (51.37%) - LeetHub

--- 1686-stone-game-vi/1686-stone-game-vi.java
+++ 1686-stone-game-vi/1686-stone-game-vi.java
@@ -1,33 +1,13 @@
 class Solution {
     public int stoneGameVI(int[] aliceValues, int[] bobValues) {
-         int n = aliceValues.length;
-        
-        int[][] stoneValues = new int[n][3];
-        
-        for(int i = 0; i < n; i++){
-            stoneValues[i] = new int[]{aliceValues[i], bobValues[i],  (aliceValues[i] + bobValues[i])};
-        }
-        
-        //Sort the stones, based on sum of Alice and Bob stones values
-        Arrays.sort(stoneValues, (a, b) -> (b[2] - a[2]));
-        
-        int aliceScore = 0;
-        
-        //Alternatively pick stone value from Alice and Bob, start with Alice
-        for(int i = 0; i < n; i++){
-            
-            if(i % 2 == 0){
-                //Append the Alice stone values
-                aliceScore += stoneValues[i][0];
-            }else{
-                //Substract the Bob stones values
-                aliceScore -= stoneValues[i][1];
-            }
-        }
-        
-        if(aliceScore > 0) return 1;
-        if(aliceScore < 0) return -1;
-        
-        return 0;
+      PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0] + b[1], a[0] + a[1]));
+    for(int i = 0; i < aliceValues.length; i++) {
+    	pq.add(new int[] { aliceValues[i], bobValues[i] });
+    }
+    int[] score = new int[2];
+    for(int i = 0; i < aliceValues.length; i++) {
+    	score[i & 1] += pq.poll()[i & 1];
+    }
+    return Integer.compare(score[0],score[1]);
     }
 }
