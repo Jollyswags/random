fdeaa9ddec84181603fa920b2aca3e8a2d7c8c46
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-27 00:54:33 +0530

    Time: 68 ms, Memory: 40 MB - LeetHub

--- clone-graph/clone-graph.java
+++ clone-graph/clone-graph.java
@@ -3,17 +3,23 @@
 class Node {
     public int val;
     public List<Node> neighbors;
-
-    public Node() {}
-
-    public Node(int _val,List<Node> _neighbors) {
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
-};
+}
 */
+
 class Solution {
-    private HashMap <Node, Node> visited = new HashMap <> ();
+   private HashMap <Node, Node> visited = new HashMap <> ();
     public Node cloneGraph(Node node) {
         if (node == null) {
             return node;
@@ -39,4 +45,3 @@ class Solution {
         return cloneNode;
     }
 }
-
