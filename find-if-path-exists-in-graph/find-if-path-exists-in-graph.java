1a11dbb611ab63709c441c790bff914c14f9fc43
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-01-26 23:13:55 +0530

    Time: 291 ms, Memory: 195.7 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,26 +1,38 @@
 class Solution {
-     public boolean validPath(int n, int[][] edges, int start, int end) {
-    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int i = 0; i < edges.length; i++) {
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
+    public boolean validPath(int n, int[][] edges, int source, int dest) {
+        HashMap<Integer, List<Integer>> map=new HashMap<>();
+        int k=0;
+        int len=edges.length;
+       // System.out.println(len);
+        for(int edge[] : edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            if(!map.containsKey(u))
+                map.put(u,new ArrayList<>());
+            map.get(u).add(v);
+            if(!map.containsKey(v))
+                map.put(v,new ArrayList<>());
+            map.get(v).add(u);
+            
         }
-        
-        Queue<Integer> q = new LinkedList<>();
-        HashSet<Integer> vis = new HashSet<>();
-        q.add(start);
-        while (!q.isEmpty()) {
-            int u = q.poll();
+        Queue<Integer> q=new LinkedList<>();
+        HashSet<Integer> vis=new HashSet<>();
+        //set.add(start);
+        q.add(source);
+        while(!q.isEmpty())
+        {
+            int u=q.poll();
             vis.add(u);
-            if (u == end) return true;
-            for (int v : adj.get(u)) {
-                if (!vis.contains(v)) {
-                    q.add(v);
-                }
+            if(u==dest)
+                return true;
+            for(int v:map.get(u))
+            {
+               if(!vis.contains(v))
+                q.add(v);
             }
         }
         return false;
+    }
+    // bfs
 }
-}
-    //use bfs
