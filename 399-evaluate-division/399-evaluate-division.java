7a31257d94c370d2edfd35ed80fc4878a93f9a15
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-02-01 17:56:23 +0530

    Time: 2 ms (41.52%), Space: 42.9 MB (5.08%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,79 +1,87 @@
-class Solution {
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        // Creating the parent graph.
-        Map<String, Pair<String, Double>> parent = new HashMap<>();
-        Map<String, Integer> size = new HashMap<>();
-        for (List<String> equation: equations) {
-            String s1 = equation.get(0);
-            String s2 = equation.get(1);
-            parent.putIfAbsent(s1, new Pair<>(s1, 1.0));
-            parent.putIfAbsent(s2, new Pair<>(s2, 1.0));
-            size.putIfAbsent(s1, 1);
-            size.putIfAbsent(s2, 1);
+class UF {
+    int[] parent;
+    int[] sz;
+    double[] weight;
+    int count;
+    public UF(int n) {
+        parent = new int[n];
+        sz = new int[n];
+        weight = new double[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            sz[i] = 1;
+            weight[i] = 1.0;
         }
-        
-        for (int i = 0; i < equations.size(); i++) {
-            String s1 = equations.get(i).get(0);
-            String s2 = equations.get(i).get(1);
-            double value = values[i];
-            union(s1, s2, value, parent, size);
+        count = n;
+    }
+    public int find(int p) {
+        if (p != parent[p]) {
+            int origp = parent[p];
+            parent[p] = find(parent[p]);
+            weight[p] *= weight[origp];
+        }
+        return parent[p];
+    }
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+    public double getQuotient(int p, int q) {
+        if (connected(p, q))
+            return weight[p] / weight[q];
+        else
+            return -1.0;
+    }
+    public void union(int p, int q, double value) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        if (sz[rootP] < sz[rootQ]) {
+            parent[rootP] = rootQ;
+            sz[rootQ] += sz[rootP];
+            weight[rootP] = weight[q] * value / weight[p];
+        }
+        else {
+            parent[rootQ] = rootP;
+            sz[rootP] += sz[rootQ];
+            weight[rootQ] = weight[p] / value / weight[q];
+        }
+        count--;
+    }
+}
+class Solution {
+    private Map<String, Integer> varIndex;
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        varIndex = new HashMap<>();
+        int M = equations.size();
+        int n = M * 2;
+        UF uf = new UF(n);
+        int id = 0;
+        for (int i = 0; i < M; i++) {
+            String v1 = equations.get(i).get(0);
+            String v2  = equations.get(i).get(1);
+            double q = values[i];
+            if (!varIndex.containsKey(v1))
+                varIndex.put(v1, id++);
+            if (!varIndex.containsKey(v2))
+                varIndex.put(v2, id++);
+            uf.union(indexOf(v1), indexOf(v2), q);
         }
 
-        // Main loop
         double[] ans = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
-            List<String> query = queries.get(i);
-            String s1 = query.get(0);
-            String s2 = query.get(1);
-            if (!parent.containsKey(s1) || !parent.containsKey(s2)) {
+            String v1 = queries.get(i).get(0);
+            String v2 = queries.get(i).get(1);
+            if (!varIndex.containsKey(v1) || !varIndex.containsKey(v2))
                 ans[i] = -1.0;
-                continue;
-            }
-            if (s1.equals(s2)) {
+            else if (v1.equals(v2))
                 ans[i] = 1.0;
-                continue;
-            }
-            String p1 = find(s1, parent).getKey();
-            String p2 = find(s2, parent).getKey();
-            if (!p1.equals(p2)) {
-                ans[i] = -1.0;
-            } else {
-                ans[i] = find(s1, parent).getValue() / find(s2, parent).getValue();
+            else {
+                ans[i] = uf.getQuotient(indexOf(v1), indexOf(v2));
             }
         }
-        
         return ans;
     }
-    
-    public Pair<String, Double> find(String p, Map<String, Pair<String, Double>> parent) {
-        String key = parent.get(p).getKey();
-        String keyP = parent.get(key).getKey();
-        while (!key.equals(keyP)) {
-            double value = parent.get(p).getValue();
-            double valueP = parent.get(key).getValue();
-            parent.put(p, new Pair<>(keyP, value * valueP));
-            key = parent.get(p).getKey();
-            keyP = parent.get(key).getKey();
-        }
-        return parent.get(p);
-    }
-    
-    public void union(String s1, String s2, double value, Map<String, Pair<String, Double>> parent, Map<String, Integer> size) {
-        Pair<String, Double> p1 = find(s1, parent);
-        Pair<String, Double> p2 = find(s2, parent);
-        String node1 = p1.getKey();
-        String node2 = p2.getKey();
-        double ratio1 = p1.getValue();
-        double ratio2 = p2.getValue();
-        int size1 = size.get(node1);
-        int size2 = size.get(node2);
-        if (node1.equals(node2)) return;
-        if (size1 >= size2) {
-            parent.put(node2, new Pair<>(node1, ratio1 / (ratio2 * value)));
-            size.put(node1, size1 + size2);
-        } else {
-            parent.put(node1, new Pair<>(node2, ratio2 * value / ratio1));
-            size.put(node2, size1 + size2);
-        }
+    private Integer indexOf(String name) {
+        return varIndex.get(name);
     }
 }
