dea22497456517c77b63a9f4bb699f2475112d57
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-02-01 23:28:58 +0530

    Time: 0 ms, Memory: 47.4 MB - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -23,43 +23,23 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        
-        if (root == null) {
-            return root;
-        }
-        
-        // Start with the root node. There are no next pointers
-        // that need to be set up on the first level
-        Node leftmost = root;
-        
-        // Once we reach the final level, we are done
-        while (leftmost.left != null) {
-            
-            // Iterate the "linked list" starting from the head
-            // node and using the next pointers, establish the 
-            // corresponding links for the next level
-            Node head = leftmost;
-            
-            while (head != null) {
-                
-                // CONNECTION 1
-                head.left.next = head.right;
-                
-                // CONNECTION 2
-                if (head.next != null) {
-                    head.right.next = head.next.left;
+        if(root==null)
+            return null;
+        Node leftmost=root;
+        while(leftmost.left!=null)
+        {
+            Node head=leftmost;
+            while(head!=null)
+            {
+                head.left.next=head.right;
+                if(head.next!=null)
+                {
+                    head.right.next=head.next.left;                
                 }
-                
-                // Progress along the list (nodes on the current level)
-                head = head.next;
+                head=head.next;
             }
-            
-            // Move onto the next level
-            leftmost = leftmost.left;
+            leftmost=leftmost.left;
         }
-        
         return root;
     }
 }
-//o(n)
-//o(1)
