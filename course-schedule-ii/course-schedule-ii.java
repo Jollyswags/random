bc46356abcb8a95740a3d3f50b155554d3a75bae
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-02-06 21:27:40 +0530

    Time: 34 ms, Memory: 47.6 MB - LeetHub

--- course-schedule-ii/course-schedule-ii.java
+++ course-schedule-ii/course-schedule-ii.java
@@ -1,54 +1,51 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-         boolean isPossible = true;
-    Map<Integer, List<Integer>> adjList = new HashMap<Integer, List<Integer>>();
-    int[] indegree = new int[numCourses];
-    int[] topologicalOrder = new int[numCourses];
-
-    // Create the adjacency list representation of the graph
-    for (int i = 0; i < prerequisites.length; i++) {
-      int dest = prerequisites[i][0];
-      int src = prerequisites[i][1];
-      List<Integer> lst = adjList.getOrDefault(src, new ArrayList<Integer>());
-      lst.add(dest);
-      adjList.put(src, lst);
-
-      // Record in-degree of each vertex
-      indegree[dest] += 1;
-    }
-
-    // Add all vertices with 0 in-degree to the queue
-    Queue<Integer> q = new LinkedList<Integer>();
-    for (int i = 0; i < numCourses; i++) {
-      if (indegree[i] == 0) {
-        q.add(i);
-      }
-    }
-
-    int i = 0;
-    // Process until the Q becomes empty
-    while (!q.isEmpty()) {
-      int node = q.remove();
-      topologicalOrder[i++] = node;
-
-      // Reduce the in-degree of each neighbor by 1
-      if (adjList.containsKey(node)) {
-        for (Integer neighbor : adjList.get(node)) {
-          indegree[neighbor]--;
-
-          // If in-degree of a neighbor becomes 0, add it to the Q
-          if (indegree[neighbor] == 0) {
-            q.add(neighbor);
-          }
+        int[] result = new int[numCourses];
+        if (numCourses == 0) {
+            return result;
         }
-      }
-    }
+        
+        if (prerequisites == null || prerequisites.length == 0) {
+            for (int i = 0; i < numCourses; i++) {
+                result[i] = i;
+            }
+            return result;
+        }
+        
+        int[] indegree = new int[numCourses];
+        Queue<Integer> zeroDegree = new LinkedList<>();
+        for (int[] pre : prerequisites) {
+            indegree[pre[0]]++;
+        }
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0){
+                zeroDegree.add(i);
+            }
+        }
+        if (zeroDegree.isEmpty()) {
+            return new int[0];
+        }
+        int index = 0;
+        while (!zeroDegree.isEmpty()) {
+            int course = zeroDegree.poll();
+            result[index] = course;
+            index++;
+            for (int[] pre : prerequisites) {
+                if (pre[1] == course) {
+                    indegree[pre[0]]--;
+                    if (indegree[pre[0]] == 0) {
+                        zeroDegree.add(pre[0]);
+                    }
+                }
+            }
+        }
+        
+        for (int in : indegree) {
+            if (in != 0) {
+                return new int[0];
+            }
+        }
 
-    // Check to see if topological sort is possible or not.
-    if (i == numCourses) {
-      return topologicalOrder;
-    }
-
-    return new int[0];
+        return result;
     }
 }
