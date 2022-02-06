ae7fe017a9b501168f311d21a403b98c136359f4
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-02-06 21:08:26 +0530

    Time: 7 ms, Memory: 43.5 MB - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,57 +1,31 @@
 class Solution {
-     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        if (flights == null || flights.length == 0 || n <= 0 || k < 0)
-            return -1;
-        List<List<Pair>> graph = buildGraph(flights, n);
-        PriorityQueue<City> minHeap = new PriorityQueue<>((city1, city2) -> Integer.compare(city1.costFromSrc, city2.costFromSrc));
-        // source, distance from src to current city, cost from src to currency city
-        minHeap.offer(new City(src, 0, 0));
-        Map<Integer, Integer> visited = new HashMap<>();
-        while (!minHeap.isEmpty()) {
-            City currentCity = minHeap.poll();
-            if (visited.containsKey(currentCity.city) && visited.get(currentCity.city) <= currentCity.distanceFromSrc)
-                continue;
-            if (currentCity.city == dst)
-                return currentCity.costFromSrc;
-            if (currentCity.distanceFromSrc > k)
-                continue;
-            visited.put(currentCity.city, currentCity.distanceFromSrc);
-            List<Pair> neighbour = graph.get(currentCity.city);
-            for (Pair pair : neighbour) {
-                minHeap.offer(new City(pair.destination, currentCity.distanceFromSrc + 1, currentCity.costFromSrc + pair.price));
-            }
+    // Bellman Ford Algorithm
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        if (src == dst) {
+            return 0;
         }
-        return -1;
-    }
-    
-    public List<List<Pair>> buildGraph(int [][] flights, int n) {
-        List<List<Pair>> graph = new ArrayList<>();
-        for (int i=0; i<n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int [] flight : flights) {
-            graph.get(flight[0]).add(new Pair(flight[1], flight[2]));
-        }
-        return graph;
-    }
-}
-class City {
-    int city;
-    // means the number of stops from source
-    int distanceFromSrc;
-    int costFromSrc;
-    public City(int city, int distanceFromSrc, int costFromSrc) {
-        this.city = city;
-        this.distanceFromSrc = distanceFromSrc;
-        this.costFromSrc = costFromSrc;
-    }
-}
 
-class Pair {
-    int destination;
-    int price;
-    public Pair(int destination, int price) {
-        this.destination = destination;
-        this.price = price;
+        int[] previous = new int[n];
+        int[] current = new int[n];
+        for (int i = 0; i < n; i++) {
+            previous[i] = Integer.MAX_VALUE;
+            current[i] = Integer.MAX_VALUE;
+        }
+        previous[src] = 0;
+        for (int i = 1; i < k + 2; i++) {
+            current[src] = 0;
+            for (int[] flight : flights) {
+                int previous_flight = flight[0];
+                int current_flight = flight[1];
+                int cost = flight[2];
+
+                if (previous[previous_flight] < Integer.MAX_VALUE) {
+                    current[current_flight] = Math.min(current[current_flight],
+                                                       previous[previous_flight] + cost);
+                }
+            }
+            previous = current.clone();
+        }
+        return current[dst] == Integer.MAX_VALUE ? -1 : current[dst];
     }
 }
