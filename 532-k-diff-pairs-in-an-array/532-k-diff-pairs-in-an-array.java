53af1ed30388b90b609c9d97476742bddf81b006
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-02-09 10:58:56 +0530

    Time: 7 ms (86.39%), Space: 42.1 MB (63.11%) - LeetHub

---
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+          HashMap<Integer, Integer> record = new HashMap<>();
+        int count = 0;
+        for (int i:nums) {
+            if (k == 0) {
+                if (record.containsKey(i) && record.get(i) == 1) {
+                    count++;
+                }
+                record.merge(i, 1, Integer::sum);
+            } else {
+                if (record.containsKey(i + k) && !record.containsKey(i)) {
+                    count++;
+                }
+                if (record.containsKey(i - k) && !record.containsKey(i)) {
+                    count++;
+                }
+                record.putIfAbsent(i, 1);
+            }
+        }
+
+        return count;
+    }
+}
