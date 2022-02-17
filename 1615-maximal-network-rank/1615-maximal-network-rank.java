832c0203c94185d14d5f447a6a31a5fe7798a862
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-02-17 23:02:38 +0530

    Time: 2 ms (100.00%), Space: 53.6 MB (26.85%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+         int[] degreeOfNodes = new int[n];
+    for (int[] road : roads) {
+      degreeOfNodes[road[0]]++;
+      degreeOfNodes[road[1]]++;
+    }
+    int maxDegree1 = 0, maxDegree2 = 0;
+    for (int node = 0; node < n; ++node) {
+      int degreeOfNode = degreeOfNodes[node];
+      if (degreeOfNode > maxDegree1) {
+        maxDegree2 = maxDegree1;
+        maxDegree1 = degreeOfNode;
+      } else if (degreeOfNode < maxDegree1 && degreeOfNode > maxDegree2){
+        maxDegree2 = degreeOfNode;
+      }
+    }
+    int nRoadMaxDegree = 0, nRoadCrossDegree = 0;
+    for (int[] road : roads) {
+      int degree1 = degreeOfNodes[road[0]];
+      int degree2 = degreeOfNodes[road[1]];
+      if (degree1 == maxDegree1 && degree2 == maxDegree1) {
+        nRoadMaxDegree++;
+      } else if (degree1 == maxDegree1 && degree2 == maxDegree2 ||
+        degree1 == maxDegree2 && degree2 == maxDegree1) {
+        nRoadCrossDegree++;
+      }
+    }
+    int nNodeOfMaxDegree1 = 0, nNodeOfMaxDegree2 = 0;
+    for (int node = 0; node < n; ++node) {
+      int degree = degreeOfNodes[node];
+      if (degree == maxDegree1) {
+        nNodeOfMaxDegree1++;
+      } else if (degree == maxDegree2) {
+        nNodeOfMaxDegree2++;
+      }
+    }
+    int maxRank = 0;
+    if (nNodeOfMaxDegree1 > 1) {
+      if (nRoadMaxDegree >= nNodeOfMaxDegree1 * (nNodeOfMaxDegree1 - 1) / 2) {
+        maxRank = Math.max(maxRank, maxDegree1 * 2 - 1);
+      } else {
+        maxRank = Math.max(maxRank, maxDegree1 * 2);
+      }
+    }
+    if (nNodeOfMaxDegree1 > 0 && nNodeOfMaxDegree2 > 0) {
+      if (nRoadCrossDegree >= nNodeOfMaxDegree1 * nNodeOfMaxDegree2) {
+        maxRank = Math.max(maxRank, maxDegree1 + maxDegree2 - 1);
+      } else {
+        maxRank = Math.max(maxRank, maxDegree1 + maxDegree2);
+      }
+    }
+    return maxRank;
+    }
+}
