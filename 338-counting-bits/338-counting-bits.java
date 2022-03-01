c5f40934fd823cb0103c467d8685c0c04bc6ae34
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-01 11:05:56 +0530

    Time: 2 ms (82.30%), Space: 48.1 MB (28.98%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int n) {
+        int bits[]=new int[n+1];
+       
+        int i;
+        for(i=0;i<=n;i++)
+        {
+            if(i%2==0)
+                bits[i]=bits[i/2];
+            else
+                bits[i]=bits[i/2]+1;
+        }
+        return bits;
+    }
+}
