9755fe8ac88a97b0afd5b3a355badf69c0617000
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-09 01:09:28 +0530

    Time: 34 ms (42.37%), Space: 66.3 MB (26.35%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,34 +1,67 @@
 class Solution {
-    public int networkDelayTime(int[][] times, int N, int K) {
-        Map<Integer, List<int[]>> graph = new HashMap();
-        for (int[] edge: times) {
-            if (!graph.containsKey(edge[0]))
-                graph.put(edge[0], new ArrayList<int[]>());
-            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
-        }
-        PriorityQueue<int[]> heap = new PriorityQueue<int[]>(
-                (info1, info2) -> info1[0] - info2[0]);
-        heap.offer(new int[]{0, K});
-
-        Map<Integer, Integer> dist = new HashMap();
-
-        while (!heap.isEmpty()) {
-            int[] info = heap.poll();
-            int d = info[0], node = info[1];
-            if (dist.containsKey(node)) continue;
-            dist.put(node, d);
-            if (graph.containsKey(node))
-                for (int[] edge: graph.get(node)) {
-                    int nei = edge[0], d2 = edge[1];
-                    if (!dist.containsKey(nei))
-                        heap.offer(new int[]{d+d2, nei});
+    // Adjacency list
+    Map<Integer, List<Pair<Integer, Integer>>> adj = new HashMap<>();
+    
+    private void dijkstra(int[] signalReceivedAt, int source, int n) {
+        Queue<Pair<Integer, Integer>> pq = new PriorityQueue<Pair<Integer,Integer>>
+            (Comparator.comparing(Pair::getKey));
+        pq.add(new Pair(0, source));
+        
+        // Time for starting node is 0
+        signalReceivedAt[source] = 0;
+        
+        while (!pq.isEmpty()) {
+            Pair<Integer, Integer> topPair = pq.remove();
+            
+            int currNode = topPair.getValue();
+            int currNodeTime = topPair.getKey();
+            
+            if (currNodeTime > signalReceivedAt[currNode]) {
+                continue;
+            }
+            
+            if (!adj.containsKey(currNode)) {
+                continue;
+            }
+            
+            // Broadcast the signal to adjacent nodes
+            for (Pair<Integer, Integer> edge : adj.get(currNode)) {
+                int time = edge.getKey();
+                int neighborNode = edge.getValue();
+                
+                // Fastest signal time for neighborNode so far
+                // signalReceivedAt[currNode] + time : 
+                // time when signal reaches neighborNode
+                if (signalReceivedAt[neighborNode] > currNodeTime + time) {
+                    signalReceivedAt[neighborNode] = currNodeTime + time;
+                    pq.add(new Pair(signalReceivedAt[neighborNode], neighborNode));
                 }
+            }
         }
-
-        if (dist.size() != N) return -1;
-        int ans = 0;
-        for (int cand: dist.values())
-            ans = Math.max(ans, cand);
-        return ans;
+    }
+    
+    public int networkDelayTime(int[][] times, int n, int k) {
+        // Build the adjacency list
+        for (int[] time : times) {
+            int source = time[0];
+            int dest = time[1];
+            int travelTime = time[2];
+            
+            adj.putIfAbsent(source, new ArrayList<>());
+            adj.get(source).add(new Pair(travelTime, dest));
+        }
+        
+        int[] signalReceivedAt = new int[n + 1];
+        Arrays.fill(signalReceivedAt, Integer.MAX_VALUE);
+        
+        dijkstra(signalReceivedAt, k, n);
+        
+        int answer = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            answer = Math.max(answer, signalReceivedAt[i]);
+        }
+        
+        // INT_MAX signifies atleat one node is unreachable
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
 }
