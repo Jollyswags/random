4f6c910f39ef67276ea073cf7033710d8cbf2058
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-08 23:15:37 +0530

    Time: 23 ms (35.31%), Space: 42.9 MB (81.63%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,26 +1,32 @@
 class Solution {
-    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    List<List<Integer>> adj = new ArrayList<>();
     public int maximalNetworkRank(int n, int[][] roads) {
-        int[] deg = new int[n];
-        for(int i = 0 ; i < n ; i++)
-            adj.add(new ArrayList<Integer>());
-        for(int[] i : roads){
-            int u = i[0];
-            int v = i[1];
+        int ans= Integer.MIN_VALUE;
+        int degree[]=new int[n];
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<>());
+        for(int road[] : roads)
+        {
+            int u = road[0];
+            int v = road[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
-            deg[u]++;
-            deg[v]++;
+            degree[u]++;
+            degree[v]++;
         }
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                int temp = deg[i] + deg[j];
-                if(adj.get(i).contains(j))
-                    temp--;
-                ans = Math.max(ans , temp);
+        for(int i=0;i<n;i++)
+            {
+                for(int j=i+1;j<n;j++)
+                {
+                    int temp = degree[i] + degree[j];
+                    if(adj.get(i).contains(j))
+                    {
+                        temp--;
+                    }
+                    ans=Math.max(ans,temp);
+                }
             }
-        }
         return ans;
+        
     }
 }
