ed10e644011664de06b5990df53c853f9971d491
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-08 23:51:10 +0530

    Time: 74 ms (24.78%), Space: 86.4 MB (24.61%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -1,45 +1,45 @@
 class Solution {
- public List<Integer> eventualSafeNodes(int[][] graphList) {
+ public List<Integer> eventualSafeNodes(int[][] graphList) 
+ {
+     int n  = graphList.length;
+     int []degree = new int[n];
+     boolean isSafe[] = new boolean[n];
+     Queue<Integer> q = new LinkedList<>();
+     HashSet<Integer>[]  neighbour= new HashSet[n];
+     for(int i=0;i<n;i++){
+       neighbour[i] = new HashSet<Integer>();
+}
+     for(int i=0;i<n;i++)
+     {
+         if(graphList[i].length==0)
+         {
+             isSafe[i]=true;
+             q.offer(i);
+         }
+         for(int nei:graphList[i])
+         {
+             neighbour[nei].add(i);
+         }
+         degree[i]=graphList[i].length;
+     }
+     while(!q.isEmpty())
+     {
+         int curr =q.poll();
+         isSafe[curr] = true;
+         for(int nei : neighbour[curr])
+         {
+             degree[nei]--;
+             if(degree[nei]==0)
+                 q.offer(nei);
+         }
+     }
+     List<Integer> ans=new ArrayList<>();
+     for(int i=0;i<n;i++)
+         if(isSafe[i])
+             ans.add(i);
+     return ans;
+ }
 
-int n  = graphList.length;
-int []degree = new int[n];
-boolean isSafe[] = new boolean[n];
-Queue<Integer> q = new LinkedList<>();
-HashSet<Integer>[]  neighbour= new HashSet[n];
-for(int i=0;i<n;i++){
-    neighbour[i] = new HashSet<Integer>();
-}
-for(int i=0;i<n;i++){
-    
-    if(graphList[i].length==0){
-        isSafe[i] = true;
-        q.offer(i);
-    }
-    for(int v:graphList[i]){
-     neighbour[v].add(i);   
-    }        
-    degree[i] = graphList[i].length;
-}
-    
-while(!q.isEmpty()){
-    
-    int curr = q.poll();
-    isSafe[curr] = true;
-    
-    for(int v:neighbour[curr]){
-     
-        degree[v]--;
-        if(degree[v]==0){
-            q.offer(v);
-        }            
-    }        
-}
-ArrayList<Integer> res = new ArrayList<>();
-for(int i=0;i<n;i++){
-    if(isSafe[i])res.add(i);
-}  
-return res;
-}
 }
 
 
