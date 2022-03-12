2733b37d3e65f42b3ff5c3c8b43743c65d8e2f52
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-12 18:56:03 +0530

    Time: 68 ms (35.92%), Space: 57.5 MB (62.52%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumDeletions(String s) {
+           int aCount = 0;
+        int bCount = 0;
+        int n = s.length();
+        
+        for (int i = n - 1; i >= 0; i--) {            
+            if (s.charAt(i) == 'b')
+                bCount++;
+        }
+        
+        if (bCount == 0 || bCount == s.length())
+            return 0;
+        
+        int maxLen = bCount;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                aCount++;
+            }
+            if (s.charAt(i) == 'b') {
+                bCount--;
+            }
+            maxLen = Math.max(maxLen, aCount + bCount);
+        }
+        
+        return s.length() - maxLen;     
+    }
+}
