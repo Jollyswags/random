d48fd0c7d876176e97257661f57deab64b29f14e
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-12 14:21:13 +0530

    Time: 2 ms (83.69%), Space: 41.8 MB (40.83%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+         Stack<Integer> stack = new Stack();
+        int result = 0;
+        
+        for(int i = 0; i < arr.length; ++i) {
+            while(!stack.empty() && stack.peek() <= arr[i])
+                result += stack.pop() * (stack.empty() ? arr[i] : Math.min(stack.peek(), arr[i]));
+            
+            stack.push(arr[i]);
+        }
+        
+        while(stack.size() > 1)
+            result += stack.pop() * stack.peek();
+        
+        return result;  
+    }
+}
