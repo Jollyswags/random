cf7886d4c79f581b5d4f1f27c1dcb47668012ea8
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-14 20:20:50 +0530

    Time: 58 ms (52.28%), Space: 139.2 MB (44.59%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int n = nums.length, ind = 0;
+        int[] res = new int[n - k + 1];
+        
+        for (int i = 0; i < n; i++) {
+            // need to keep the queue in monotinically decreasing
+            // keep a queue in decreasing order, as on the front(first) there must be maximum
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i])
+                deque.pollLast();
+            
+            // add the number at last, which will be smaller then all previous number in the queue
+            deque.addLast(i);
+            
+            // need to remove al the element fron the front which are NOT part of window
+            // that's why we are using index
+            while (!deque.isEmpty() && (i - deque.peekFirst()) >= k)
+                deque.pollFirst();
+            
+            // update the res, once we have index >= k - 1, 
+            // window of k, index will be k - 1 and greater
+            if (i >= (k - 1))
+                res[ind++] = nums[deque.peekFirst()];
+        }
+        return res;
+    }
+}
