5b59fc99c9854f369ffd58c0e08bd70e1895188c
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-15 20:42:57 +0530

    Time: 2 ms (88.20%), Space: 68.6 MB (20.30%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length-1;
+        int i;
+        int goodindex=n;
+        for(i=n;i>=0;i--)
+        {
+            if(i+nums[i]>=goodindex)
+            {
+                goodindex=i;
+            }
+        }
+        return goodindex==0;
+        
+    }
+}
