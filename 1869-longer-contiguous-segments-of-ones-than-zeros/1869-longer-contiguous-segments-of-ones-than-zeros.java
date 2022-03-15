381f4803d52929c03d8c7a56400fd118d3422973
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-15 22:21:42 +0530

    Time: 1 ms (82.03%), Space: 42.2 MB (21.66%) - LeetHub

---
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        var maxZeros = 0;
+	var maxOnes = 0;
+
+	for (int i = 0, zeros = 0, ones = 0; i < s.length(); i++)
+		if (s.charAt(i) == '0') {
+			maxZeros = Math.max(maxZeros, ++zeros);
+			ones = 0;
+		} else {
+			maxOnes = Math.max(maxOnes, ++ones);
+			zeros = 0;
+		}
+
+	return maxOnes > maxZeros;
+    }
+}
