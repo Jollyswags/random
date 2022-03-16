eb8b11086b712712309757a709bc6047f02afd6a
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-17 01:04:33 +0530

    Time: 14 ms (93.45%), Space: 44.2 MB (81.17%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,44 +1,47 @@
 class Solution {
-       private boolean isValid(int r, int c, int R, int C) {
+    private boolean isValid(int r, int c, int R, int C) {
         return 0 <= r && r < R && 0 <= c && c < C;
     }
-
     public int shortestPathBinaryMatrix(int[][] grid) {
-
-        if (grid == null || grid.length == 0 || grid[0][0] == 1) {
+         if (grid == null || grid.length == 0 || grid[0][0] == 1) {
             return -1;
         }
         int[] dr = {-1, -1, -1, 1, 1, 1, 0, 0};
         int[] dc = {-1, 0, 1, -1, 0, 1, 1, -1};
-        int ROW = 0;
-        int COL = 1;
         int R = grid.length;
         int C = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 0});
-        int distance = 1;
-        int currLevel = 1;
-        int nextLevel = 0;
-        while (!queue.isEmpty()) {
-            int[] curr = queue.remove();
-            currLevel--;
-            if (curr[0] == R - 1 && curr[1] == C - 1) {
-                return distance;//reached end
-            }
-            for (int k = 0; k < 8; k++) {
-                int fr = dr[k] + curr[ROW];
-                int fc = dc[k] + curr[COL];
-                if (isValid(fr, fc, R, C) && grid[fr][fc] == 0) {
-                    grid[fr][fc] = 1;
-                    nextLevel++;
-                    queue.add(new int[]{fr, fc});
+        boolean vis[][]=new boolean[R][C];
+        vis[0][0]=true;
+        int dist=1;
+        while(!queue.isEmpty())
+        {
+            int s=queue.size();;
+            for(int i=0;i<s;i++)
+            {
+                int mat[]=queue.poll();
+                if(mat[0]==R-1 && mat[1]==C-1)
+                    return dist;
+                for(int j=0;j<8;j++)
+                {
+                    int x=dr[j]+mat[0];
+                    int y=dc[j]+mat[1];
+                    if(isValid(x,y,R,C))
+                    {
+                         if(vis[x][y])
+                            continue;
+                        if(!vis[x][y] && grid[x][y]==0)
+                        {
+                            vis[x][y]=true;
+                             queue.offer(new int[]{x,y});
+                        }
+                    }
+                    
                 }
             }
-            if (currLevel == 0) {
-                currLevel = nextLevel;
-                nextLevel = 0;
-                distance++;
-            }
+            
+            dist++;
         }
         return -1;
     }
