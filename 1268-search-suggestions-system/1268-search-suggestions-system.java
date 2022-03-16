96cd69d0e944785dafb8ab3660772b155b36995b
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-16 15:16:40 +0530

    Time: 57 ms (48.80%), Space: 75.4 MB (34.83%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,70 +1,35 @@
-// Custom class Trie with function to get 3 words starting with given prefix
-class Trie {
-
-    // Node definition of a trie
-    class Node {
-        boolean isWord = false;
-        List<Node> children = Arrays.asList(new Node[26]);
-    };
-    Node Root, curr;
-    List<String> resultBuffer;
-
-    // Runs a DFS on trie starting with given prefix and adds all the words in the resultBuffer, limiting result size to 3
-    void dfsWithPrefix(Node curr, String word) {
-        if (resultBuffer.size() == 3)
-            return;
-        if (curr.isWord)
-            resultBuffer.add(word);
-
-        // Run DFS on all possible paths.
-        for (char c = 'a'; c <= 'z'; c++)
-            if (curr.children.get(c - 'a') != null)
-                dfsWithPrefix(curr.children.get(c - 'a'), word + c);
-    }
-    Trie() {
-        Root = new Node();
-    }
-
-    // Inserts the string in trie.
-    void insert(String s) {
-
-        // Points curr to the root of trie.
-        curr = Root;
-        for (char c : s.toCharArray()) {
-            if (curr.children.get(c - 'a') == null)
-                curr.children.set(c - 'a', new Node());
-            curr = curr.children.get(c - 'a');
-        }
-
-        // Mark this node as a completed word.
-        curr.isWord = true;
-    }
-    List<String> getWordsStartingWith(String prefix) {
-        curr = Root;
-        resultBuffer = new ArrayList<String>();
-        // Move curr to the end of prefix in its trie representation.
-        for (char c : prefix.toCharArray()) {
-            if (curr.children.get(c - 'a') == null)
-                return resultBuffer;
-            curr = curr.children.get(c - 'a');
-        }
-        dfsWithPrefix(curr, prefix);
-        return resultBuffer;
-    }
-};
 class Solution {
-    List<List<String>> suggestedProducts(String[] products,
-                                         String searchWord) {
-        Trie trie = new Trie();
-        List<List<String>> result = new ArrayList<>();
-        // Add all words to trie.
-        for (String w : products)
-            trie.insert(w);
-        String prefix="";
-        for (char c : searchWord.toCharArray()) {
-            prefix += c;
-            result.add(trie.getWordsStartingWith(prefix));
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+          String str="";
+        List<List<String>> res=new ArrayList<>();
+        for(int i=0;i<searchWord.length();i++)
+        {
+            
+             PriorityQueue<String> pq = new PriorityQueue<>((a,b)->(a.compareTo(b)));
+           str+=searchWord.charAt(i);
+            for(String p:products)
+            {
+                if(p.length()>=str.length())
+                {
+                    String temp=p.substring(0,str.length());
+                if(str.equals(temp))
+                    pq.add(p);
+                }
+                    
+            }
+            List<String> ans=new ArrayList<>();
+            int l=0;
+            if(pq.size()>3)
+                l=3;
+            else
+                l=pq.size();
+            for(int j=0;j<l;j++)
+                ans.add(pq.poll());
+            res.add(ans);
         }
-        return result;
+  
+        return res;
     }
 }
+//TC : O(N*sw.length)
+//SC: O(N)
