d3dfca43ae5ffbb741189556cf47b7c5b60c4148
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-17 19:10:13 +0530

    Time: 12 ms (7.72%), Space: 48 MB (18.56%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,41 +1,36 @@
 class Solution {
-    int N = 55*55;
-    int[] sz = new int[N];
-    int[] f = new int[N] ;
-    public int find(int x){
-        if(f[x] ==x) return x;
-        return f[x]= find(f[x]);
-    }
-     public int maxAreaOfIsland(int[][] grid) {
-        int n= grid.length;
-        int m = grid[0].length;
-        int[] dx = new int[]{0,1,0,-1};
-        int[] dy = new int[]{1,0,-1,0};
-        for(int i = 0 ; i<m*n;i++)
-        {f[i] = i ;
-         sz[i]=1;
-         }
-         for(int i =0; i<n;i++){
-            for(int j = 0;j<m;j++ ){
-                if(grid[i][j]==0) continue;
-                for(int d = 0; d<4;d++){
-                    int tx = i+dx[d];
-                    int ty = j + dy[d];
-                    if(tx<0||ty<0||tx>=n||ty>=m) continue;
-                    if(grid[tx][ty]==0) continue;
-                    int pa = find(tx*m+ty);
-                    int pb = find(i*m+j);
-                    if(pa!=pb){
-                        sz[pb]+=sz[pa];
-                        f[pa] = pb; 
+    public int maxAreaOfIsland(int[][] grid) {
+        boolean[][] seen = new boolean[grid.length][grid[0].length];
+        int[] dr = new int[]{1, -1, 0, 0};
+        int[] dc = new int[]{0, 0, 1, -1};
+
+        int ans = 0;
+        for (int r0 = 0; r0 < grid.length; r0++) {
+            for (int c0 = 0; c0 < grid[0].length; c0++) {
+                if (grid[r0][c0] == 1 && !seen[r0][c0]) {
+                    int shape = 0;
+                    Stack<int[]> stack = new Stack();
+                    stack.push(new int[]{r0, c0});
+                    seen[r0][c0] = true;
+                    while (!stack.empty()) {
+                        int[] node = stack.pop();
+                        int r = node[0], c = node[1];
+                        shape++;
+                        for (int k = 0; k < 4; k++) {
+                            int nr = r + dr[k];
+                            int nc = c + dc[k];
+                            if (0 <= nr && nr < grid.length &&
+                                    0 <= nc && nc < grid[0].length &&
+                                    grid[nr][nc] == 1 && !seen[nr][nc]) {
+                                stack.push(new int[]{nr, nc});
+                                seen[nr][nc] = true;
+                            }
+                        }
                     }
+                    ans = Math.max(ans, shape);
                 }
             }
         }
-         int res = 0;
-         for(int i = 0; i<m*n;i++){
-             if(grid[i / m][i % m] == 1) res = Math.max(res, sz[find(i)]);
-         }
-         return res;
+        return ans;
     }
 }
