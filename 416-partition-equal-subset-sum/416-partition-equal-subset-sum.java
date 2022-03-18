f4cb6396279018bcb8ace888b67f9ac61faab331
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-19 01:57:48 +0530

    Time: 39 ms (68.65%), Space: 42.4 MB (83.80%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,27 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalsum=0;
-        for(int n:nums)
-            totalsum+=n;
-        if(totalsum%2!=0)
-            return false;
-        int subsetsum=totalsum/2;
-        int n=nums.length;
-        boolean dp[][]=new boolean[n+1][subsetsum+1];
-        dp[0][0]=true;
-        int i,j;
-        for(i=1;i<=n;i++)
-        {
-            int currsum=nums[i-1];
-            for(j=0;j<=subsetsum;j++)
-            {
-                if(j<currsum)
-                    dp[i][j]=dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j] || dp[i-1][j-currsum];
-            }
-        }
-        return dp[n][subsetsum];
-        
+        int sum=0;
+		for(int i:nums) sum+=i;
+		if(sum%2!=0) return false;
+		return find(nums,sum/2);
+
+	}
+	public boolean find(int []nums,int t){
+		boolean dp[]=new boolean[t+1];
+		dp[0]=true;
+		for(int i=0;i<nums.length;i++){
+			for(int j=t;j>0;j--){
+				if(j>=nums[i]) dp[j]|= dp[j-nums[i]]; 
+			}
+		}
+		return dp[t];
     }
 }
