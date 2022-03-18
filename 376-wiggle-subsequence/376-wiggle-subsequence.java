a2fcb26aa20ca9e73fcb7e4b7ebf12508d4765de
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-18 23:51:58 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (25.24%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,20 +1,14 @@
-class Solution {
+public class Solution {
     public int wiggleMaxLength(int[] nums) {
-        int n=nums.length;
-        if(n<2)
-            return n;
-        int i,up=1,down=1;
-        for(i=1;i<n;i++)
-        {
-            if(nums[i]>nums[i-1])
-                up=down+1;
-            else
-                if(nums[i]<nums[i-1])
-                    down=up+1;
+        if (nums.length < 2)
+            return nums.length;
+        int down = 1, up = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1])
+                up = down + 1;
+            else if (nums[i] < nums[i - 1])
+                down = up + 1;
         }
-        return Math.max(up,down);
-        
-     
-        
+        return Math.max(down, up);
     }
 }
