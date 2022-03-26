7e96b95b16c5e08bdd2dc2c401c8abc219d47776
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-26 21:43:36 +0530

    Time: 31 ms (88.46%), Space: 44.4 MB (87.82%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.java
+++ 1690-stone-game-vii/1690-stone-game-vii.java
@@ -1,20 +1,28 @@
 class Solution {
+   private int[] preSum;
+    private int[] dp;
+    
     public int stoneGameVII(int[] stones) {
-         int[][] dp = new int[stones.length][stones.length];
-        int[] pre = new int[stones.length];
-        pre[0] = stones[0];
-        for(int i = 1; i < pre.length; i++)
-            pre[i] = pre[i - 1] + stones[i];
-        for(int g = 0; g < dp.length; g++){
-            for(int i = 0, j = g; j < dp.length; i++, j++){
-                if(g == 0)
-                    dp[i][j] = 0;
-                else if(g == 1)    
-                    dp[i][j] = Math.max(stones[i], stones[j]);
-                else
-                    dp[i][j] = Math.max((pre[j] - pre[i] - dp[i + 1][j]), (pre[j-1] - (i == 0 ? 0 : pre[i-1]) - dp[i][j - 1]));
+        int size = stones.length;
+        preSum = new int[size + 1];
+        dp = new int[size + 1];
+        
+        for(int i = 0; i < size; i++)
+            preSum[i + 1] = preSum[i] + stones[i];
+        
+        for(int end = 1; end <= size; end++) {
+            for(int i = 0; i < size - end; i++) {
+                dp[i] = Math.max(
+                    getScore(i + 1, i + end) - dp[i + 1], // As if we pick i, we need to find solution of (i+1, end).
+                    getScore(i, i + end - 1) - dp[i] // As if we pick end, we need to find solution of (i, end - 1)
+                ); 
             }
         }
-        return dp[0][stones.length - 1];
+        
+        return dp[0];
+    }
+    
+    private int getScore(int left, int right) {
+        return preSum[right + 1] - preSum[left];
     }
 }
