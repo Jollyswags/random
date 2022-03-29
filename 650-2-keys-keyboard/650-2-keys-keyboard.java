e91cffa559aa65d0e96c3ee996d52a69f401658b
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-03-29 17:24:49 +0530

    Time: 0 ms (100.00%), Space: 41 MB (58.33%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minSteps(int n) {
-          int ans = 0, d = 2;
+        int ans = 0, d = 2;
         while (n > 1) {
             while (n % d == 0) {
                 ans += d;
