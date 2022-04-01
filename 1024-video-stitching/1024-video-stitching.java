3f46b241d1db736b63d88229c5aee8855067b917
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-01 16:36:39 +0530

    Time: 2 ms (51.20%), Space: 42.1 MB (34.65%) - LeetHub

---
+++ 1024-video-stitching/1024-video-stitching.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int videoStitching(int[][] clips, int time) {
+        Arrays.sort(clips, (a, b) -> a[0] - b[0]);
+        
+        int cur = 0;
+        int low = 0;
+        int count = 0;
+        while (cur < clips.length && clips[cur][0] <= low && low < time) {
+            int maxUp = 0;
+            
+            while (cur < clips.length && clips[cur][0] <= low) {
+                maxUp = Math.max(maxUp, clips[cur][1]);
+                cur++;
+            }
+            
+            count++;
+            low = maxUp;
+        }
+        
+        return low >= time ? count : -1;
+    }
+}
