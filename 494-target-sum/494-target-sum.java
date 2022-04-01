c98e073cd435226562538491e6dd48718cd6f454
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-01 23:54:52 +0530

    Time: 21 ms (67.67%), Space: 52 MB (25.10%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,19 +1,21 @@
 public class Solution {
     public int findTargetSumWays(int[] nums, int S) {
         int total = Arrays.stream(nums).sum();
-        int[][] dp = new int[nums.length][2 * total + 1];
-        dp[0][nums[0] + total] = 1;
-        dp[0][-nums[0] + total] += 1;
+        int[] dp = new int[2 * total + 1];
+        dp[nums[0] + total] = 1;
+        dp[-nums[0] + total] += 1;
         
         for (int i = 1; i < nums.length; i++) {
+            int[] next = new int[2 * total + 1];
             for (int sum = -total; sum <= total; sum++) {
-                if (dp[i - 1][sum + total] > 0) {
-                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];
-                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                if (dp[sum + total] > 0) {
+                    next[sum + nums[i] + total] += dp[sum + total];
+                    next[sum - nums[i] + total] += dp[sum + total];
                 }
             }
+            dp = next;
         }
         
-        return Math.abs(S) > total ? 0 : dp[nums.length - 1][S + total];
+        return Math.abs(S) > total ? 0 : dp[S + total];
     }
 }
