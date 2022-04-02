f989aafd1749f836acd1764a8e085e6d2ca838d9
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-02 16:24:08 +0530

    Time: 53 ms (86.84%), Space: 167.8 MB (11.28%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -1,15 +1,6 @@
 class Solution {
     public int maxTwoEvents(int[][] events) {
-        // -sort the events by start time
-        // -rely on the fact that for any event events[i], all events after events[i] start at a time
-        // after events[i] and so do not overlap with any previous events not overlapping with events[i].
-        // hence, we can maintain a PQ of end times for events to keep track of which events don't overlap with
-        // the current event,
-        // and maintain a maximum-valued previous event to combine with curr events[i]
-        // answer will either be using two or one event sine value is nonnegative
-        
-        // sort events by start time
-        Arrays.sort(events, (event1, event2) -> Integer.compare(event1[0], event2[0]));
+         Arrays.sort(events, (event1, event2) -> Integer.compare(event1[0], event2[0]));
         
         // maintaining the end times of previous events to use for checking overlap
         PriorityQueue<int[]> endTimes = new PriorityQueue<>((event1, event2) -> Integer.compare(event1[1], event2[1]));
