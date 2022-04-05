a65d86897950472c3532eb53db25ea0c482b2908
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-05 16:41:48 +0530

    Time: 23 ms (60.39%), Space: 47.7 MB (55.09%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
@@ -1,19 +1,21 @@
 class Solution {
     public int numSplits(String s) {
-        int len=s.length();
-        int[] left=new int[len];
-        int[] right=new int[len];
-        Set<Character> leftchar=new HashSet();
-        Set<Character> rightchar=new HashSet();
-        for(int i=0;i<len;i++)
+       int len=s.length();
+        int i;
+        Set<Character> leftl=new HashSet<>();
+        Set<Character> rightl=new HashSet<>();
+        int left[]=new int[len];
+        int right[]=new int[len];
+        for(i=0;i<len;i++)
         {
-            leftchar.add(s.charAt(i));//left to right
-            rightchar.add(s.charAt(len-1-i));//right to left
-            left[i]=leftchar.size();
-            right[len-1-i]=rightchar.size();
+            leftl.add(s.charAt(i));
+            rightl.add(s.charAt(len-1-i));
+            left[i]=leftl.size();
+            right[len-1-i]=rightl.size();
+            
         }
         int c=0;
-        for(int i=1;i<len;i++)
+        for(i=1;i<len;i++)
         {
             if(left[i-1]==right[i])
                 c++;
