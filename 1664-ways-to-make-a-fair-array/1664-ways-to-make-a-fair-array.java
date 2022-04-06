c5d8ac3f79d654b287134ac14ae3d1d33ac8a8e1
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-06 23:15:24 +0530

    Time: 22 ms (14.07%), Space: 88.8 MB (56.70%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -1,49 +1,38 @@
 class Solution {
     public int waysToMakeFair(int[] nums) {
-       int count = 0;
-        
-        int evenLeft = 0;
-        int oddLeft = 0;
-        
-        int evenRight = 0;
-        int oddRight = 0;
-
+        int count =0;
+        int evenleft=0;
+        int evenright=0;
+        int oddleft=0;
+        int oddright=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(i%2 == 0)
-            {
-                evenRight+=nums[i];
-            } else{
-                oddRight+= nums[i];
-            }
+            if(i%2==0)
+                evenright+=nums[i];
+            else
+                oddright+=nums[i];
         }
-      
         for(int i=0;i<nums.length;i++)
         {
-            
             if(i>0)
             {
-                if(i%2 == 0)
-                {
-                     oddLeft +=nums[i-1];
-                } else{
-                     evenLeft+=nums[i-1];
-                }
+                if(i%2==0)
+                    oddleft+=nums[i-1];
+                else
+                    evenleft+=nums[i-1];
             }
-           
-            if(i%2 == 0)
+            if(i%2==0)
             {
-                evenRight-=nums[i];
-            } else{
-                oddRight-=nums[i];
+                evenright-=nums[i];
             }
-            
-           
-             if(evenLeft+oddRight == oddLeft+evenRight)
-                {
-                    count++;
-                }
+            else
+            {
+                oddright-=nums[i];
+            }
+            if(oddleft+evenright==evenleft+oddright)
+                count++;
         }
         return count;
+        
     }
 }
