edcb37cb18dfb501a55573179faf54b7762355ca
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-19 20:50:57 +0530

    Time: 0 ms (100.00%), Space: 60.1 MB (65.66%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,19 +1,35 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int l = 0;
-        int h = nums.length-1;
-        
-        while(l<h){
-            int mid = l+(h-l)/2;      // divide the array
-            
-            if(nums[mid]==nums[mid+1]) mid = mid-1;      //two same elements should be in same half
-            
-            if((mid-l+1)%2!=0) h = mid;            // checking the length of left half. If its is odd then update ur right pointer to mid
-            
-            else l = mid+1;    // else your right half will be odd then update your left pointer to mid+1
+        if(nums.length==1){
+            return nums[0];
         }
         
-        return nums[l];     //left pointer will have the answer at last
+        int lo = 0,hi = nums.length-1;
+        while(lo<=hi){
+            int mid = lo +(hi-lo)/2;
+            
+            
+            //means mid is second element of the pair
+            if(mid-1>=0 &&(nums[mid]==nums[mid-1])){ 
+                //check if mid is at odd pos
+                if((mid&1)==1){
+                    lo = mid+1;
+                }else{
+                    hi = mid;
+                }
+                
+            }else if(mid+1<nums.length &&(nums[mid]==nums[mid+1])){ //means mid is first element of the pair
+                //check if mid is at even pos
+                if((mid&1)==0){
+                    lo = mid+2;
+                }else{
+                    hi = mid;
+                }
+                
+            }else{ //means mid is single
+                return nums[mid];
+            }
+        }
+        return -1;
     }
 }
