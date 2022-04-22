0783e9da48a0ebe238d6004fe5185e08e74a0358
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-22 21:57:32 +0530

    Time: 7 ms (66.99%), Space: 42.7 MB (45.63%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -7,11 +7,12 @@ class Solution {
     for(int i=1;i<=n;i++){
         int max=0;
         int val=0;
-        for(int j=1;j<=Math.min(i,k);j++){
-            max=Math.max(max,arr[i-j]);
-            
-            val=Math.max(dp[i-j]+max*j,val);
-            
+        for(int j=1;j<=Math.min(i,k);j++)
+        {
+            max=Math.max(max,arr[i-j]);  
+            //max=Math.max(max,arr[i-j])
+            val=Math.max(dp[i-j]+max*j,val);     
+            //val=Math.max(val,dp[i-j]+max*j)
         }
         dp[i]=val;
     }
