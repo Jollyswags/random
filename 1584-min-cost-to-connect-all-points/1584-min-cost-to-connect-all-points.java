77c835c0041e9f5a15f523aa4ea0d63a86bb00da
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-23 00:35:08 +0530

    Time: 42 ms (97.15%), Space: 43.5 MB (97.59%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,67 +1,46 @@
 class Solution {
-    // Prim Algorithm
     public int minCostConnectPoints(int[][] points) {
-        if (points == null || points.length == 0) {
-            return 0;
+        int n = points.length;
+        int mstCost = 0;
+        int edgesUsed = 0;
+        
+        // Track nodes which are visited.
+        boolean[] inMST = new boolean[n];
+        
+        int[] minDist = new int[n];
+        minDist[0] = 0;
+        
+        for (int i = 1; i < n; ++i) {
+            minDist[i] = Integer.MAX_VALUE;
         }
-        int size = points.length;
-        PriorityQueue<Edge> pq = new PriorityQueue<>((x, y) -> x.cost - y.cost);
-        boolean[] visited = new boolean[size];
-        int result = 0;
-        int count = size - 1;
-        // Add all edges from points[0] vertexs
-        int[] coordinate1 = points[0];
-        for (int j = 1; j < size; j++) {
-            // Calculate the distance between two coordinates.
-            int[] coordinate2 = points[j];
-            int cost = Math.abs(coordinate1[0] - coordinate2[0]) + 
-                       Math.abs(coordinate1[1] - coordinate2[1]);
-            Edge edge = new Edge(0, j, cost);
-            pq.add(edge);
-        }
-        visited[0] = true;
-
-        while (!pq.isEmpty() && count > 0) {
-            Edge edge = pq.poll();
-            int point1 = edge.point1;
-            int point2 = edge.point2;
-            int cost = edge.cost;
-            if (!visited[point2]) {
-                result += cost;
-                visited[point2] = true;
-                for (int j = 0; j < size; j++) {
-                    if (!visited[j]) {
-                        int distance = Math.abs(points[point2][0] - points[j][0]) + 
-                                       Math.abs(points[point2][1] - points[j][1]);
-                        pq.add(new Edge(point2, j, distance));
-                    }
+        
+        while (edgesUsed < n) {
+            int currMinEdge = Integer.MAX_VALUE;
+            int currNode = -1;
+            
+            // Pick least weight node which is not in MST.
+            for (int node = 0; node < n; ++node) {
+                if (!inMST[node] && currMinEdge > minDist[node]) {
+                    currMinEdge = minDist[node];
+                    currNode = node;
                 }
-               // count--;
+            }
+            
+            mstCost += currMinEdge;
+            edgesUsed++;
+            inMST[currNode] = true;
+            
+            // Update adjacent nodes of current node.
+            for (int nextNode = 0; nextNode < n; ++nextNode) {
+                int weight = Math.abs(points[currNode][0] - points[nextNode][0]) + 
+                             Math.abs(points[currNode][1] - points[nextNode][1]);
+                
+                if (!inMST[nextNode] && minDist[nextNode] > weight) {
+                    minDist[nextNode] = weight;
+                }
             }
         }
-        return result;
-    }
-
-    class Edge {
-        int point1;
-        int point2;
-        int cost;
-
-        Edge(int point1, int point2, int cost) {
-            this.point1 = point1;
-            this.point2 = point2;
-            this.cost = cost;
-        }
+        
+        return mstCost;
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
-        int[][] points = {{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}};
-        Solution solution = new Solution();
-        System.out.print("Minimum Cost to Connect Points = "); 
-        System.out.println(solution.minCostConnectPoints(points)); 
-    }
-}
-//tc O(ElogV)
-//sc O(V)
