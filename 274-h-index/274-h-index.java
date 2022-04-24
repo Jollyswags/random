66c02d4deb54225cbda1b992a330fa8f5e9e3102
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-24 17:34:43 +0530

    Time: 3 ms (39.08%), Space: 42.1 MB (41.12%) - LeetHub

---
+++ 274-h-index/274-h-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] c) {
+        Arrays.sort(c);
+        int i = 1;
+
+        while (i<= c.length) {
+            if (c[c.length -i] < i) {
+                break;
+            }
+            i++;
+        }
+        return i-1;
+    }
+}
+//n
+//1
