337fea1abe19a1d52f27979c18ad25c2e678b4ba
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-25 03:13:28 +0530

    Time: 60 ms (15.32%), Space: 67.6 MB (14.07%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void backtrack(int start,LinkedList<Integer> current,int n,int k, List<List<Integer>> ans)
+    {
+        if(current.size() == k)
+        {
+            ans.add(new LinkedList(current));
+            return;
+        }
+        for(int i = start ; i<=n;i++)
+        {
+            current.add(i);
+            backtrack(i+1,current,n,k,ans);
+            current.removeLast();
+        }
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new LinkedList <>();
+        if(k == 0)
+        {
+            ans.add(new LinkedList<>());
+            return ans;
+        }
+        backtrack(1,new LinkedList <>(),n,k,ans);
+        return ans;
+    }
+}
