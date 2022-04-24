ed91d69762eb5068d9b7772a3770c472af4e30e7
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-25 02:21:43 +0530

    Time: 2 ms (90.39%), Space: 44.3 MB (87.36%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -1,24 +1,22 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        int N = seats.length;
-        int[] left = new int[N], right = new int[N];
-        Arrays.fill(left, N);
-        Arrays.fill(right, N);
+         int N = seats.length;
+        int prev = -1, future = 0;
+        int ans = 0;
 
         for (int i = 0; i < N; ++i) {
-            if (seats[i] == 1) left[i] = 0;
-            else if (i > 0) left[i] = left[i-1] + 1;
+            if (seats[i] == 1) {
+                prev = i;
+            } else {
+                while (future < N && seats[future] == 0 || future < i)
+                    future++;
+
+                int left = prev == -1 ? N : i - prev;
+                int right = future == N ? N : future - i;
+                ans = Math.max(ans, Math.min(left, right));
+            }
         }
 
-        for (int i = N-1; i >= 0; --i) {
-            if (seats[i] == 1) right[i] = 0;
-            else if (i < N-1) right[i] = right[i+1] + 1;
-        }
-
-        int ans = 0;
-        for (int i = 0; i < N; ++i)
-            if (seats[i] == 0)
-                ans = Math.max(ans, Math.min(left[i], right[i]));
         return ans;
     }
 }
