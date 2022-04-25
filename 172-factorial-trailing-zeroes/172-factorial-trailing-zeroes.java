af255a07bf8c66bb3adcab54a2995b89104287d6
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-04-25 19:56:33 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (78.96%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -2,8 +2,8 @@ class Solution {
     public int trailingZeroes(int n) {
            int r = 0;
         while (n > 0) {
-            n /= 5;
-            r += n;
+         n/=5;
+            r+=n;
         }
         return r;
     }
