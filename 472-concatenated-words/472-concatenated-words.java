62c34058793e2c0d7a4ae840c7c6ae5b11ce7c4e
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-05-05 18:57:41 +0530

    Time: 71 ms (76.81%), Space: 84.5 MB (23.76%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -1,34 +1,60 @@
 class Solution {
-    public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> result = new ArrayList<>();
-        Set<String> set = new HashSet<>();
-        for(String word: words)
-            set.add(word);
-        for(String word: words){
-            if(helper(word, set)){
-                result.add(word);
-            }
-        }
-        return result;
-    }
-    private boolean helper(String s, Set<String> set){
-        int len = s.length();
-        if(len == 0)
-            return false;
-        boolean[] dp = new boolean[len + 1];
-        dp[0] = true;
-        for(int i = 0; i <= len; i++){
-            for(int j = i - 1 ; j >= 0; j--){ // running this loop in the backwards direction saves time
-                if(i == len && j == 0) // we can't consider the string to be made up of itself i.e we ignore s.substring(0, len)
-                    continue;
-                if(dp[j] && set.contains(s.substring(j,i))){
-                    dp[i] = true;
-                    break;
-                }                
-            }
-        }
-        return dp[len];
-    }
+class Trie {
+        
+	class TrieNode {
+		TrieNode[] children = new TrieNode[26];
+		String word;
+	}
+
+	TrieNode root = new TrieNode();
+
+	void addWord(String word) {
+		TrieNode cur = root;
+		for (int i = 0; i < word.length(); i++) {
+			int c = word.charAt(i) - 'a';
+			if (cur.children[c] == null) {
+				cur.children[c] = new TrieNode();
+			}
+			cur = cur.children[c];
+		}
+		cur.word = word;
+	}
+
+	List<String> findAll(String[] words) {
+		List<String> res = new ArrayList<>();
+
+		for (String word : words) {
+			if (backtrack(0, "", word, res, false)) {
+				res.add(word);
+			}
+		}
+
+		return res;
+	}
+
+	private boolean backtrack(int start, String curStr, String word, List<String> res, boolean isConcat) {
+		// Base case
+		if (start == word.length()) {
+			return isConcat;
+		}
+
+		TrieNode cur = root;
+		for (int i = start; i < word.length(); i++) {
+			int c = word.charAt(i) - 'a';
+			if (cur.children[c] == null) return false;
+			if (cur.children[c].word != null) {
+				if (backtrack(i + 1, curStr + cur.children[c].word, word, res, i == word.length() - 1 ? isConcat : true)) return true;
+			}
+			cur = cur.children[c];
+		}
+		return false;
+	}
+
 }
-//TC:O(NMN)
-//SC:O(N*N)
+
+public List<String> findAllConcatenatedWordsInADict(String[] words) {
+	Trie trie = new Trie();
+	for (String word : words) trie.addWord(word);
+	return trie.findAll(words);
+}
+}
