7991a97e1f7a304b49905d27fd0a13d7e02f47c0
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-05-06 01:26:03 +0530

    Time: 395 ms (5.02%), Space: 124.6 MB (51.10%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -1,37 +1,27 @@
 class MedianFinder {
-    
-    PriorityQueue<Integer> left; // 4,3,2,1
-    PriorityQueue<Integer> right; //1,2,3,4
-    
 
-    public MedianFinder() {
-        left = new PriorityQueue<>((a, b)->b-a);
-        right = new PriorityQueue<>();
-    }
-    
-    public void addNum(int num) { //O(logN)
-        if (left.size() == 0) {
-            left.offer(num);
-        } else if (num > left.peek()) { //O(1)
-            right.offer(num);
-            if (right.size() == left.size() + 2) { //adjust
-                left.offer(right.poll());
-            }
-        } else {
-            left.offer(num);
-            if (left.size() == right.size() + 2) { // adjust
-                right.offer(left.poll());
-            }
+     // max queue is always larger or equal to min queue
+    PriorityQueue<Integer> min = new PriorityQueue();
+    PriorityQueue<Integer> max = new PriorityQueue(1000, Collections.reverseOrder());
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        max.offer(num);
+        min.offer(max.poll());
+        if (max.size() < min.size()){
+            max.offer(min.poll());
         }
     }
-    
+
+    // Returns the median of current data stream
     public double findMedian() {
-        if (left.size() == right.size()) {
-            return (left.peek() + right.peek()+0.0) / 2;
-        } else if (left.size() > right.size()) {
-            return (left.peek() + 0.0);
-        } else {
-            return (right.peek() + 0.0);
-        }
+        if (max.size() == min.size()) return (max.peek() + min.peek()) /  2.0;
+        else return max.peek();
     }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
