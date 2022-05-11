95eac0d6245c35099f742d0e9b7d9d19c569681e
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-05-11 15:34:28 +0530

    Time: 87 ms (32.77%), Space: 77.7 MB (55.88%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -4,7 +4,8 @@ class Solution {
         int[][] memo = new int[A.length + 1][B.length + 1];
         for (int i = A.length - 1; i >= 0; --i) {
             for (int j = B.length - 1; j >= 0; --j) {
-                if (A[i] == B[j]) {
+                if (A[i] == B[j])
+                {
                     memo[i][j] = memo[i+1][j+1] + 1;
                     if (ans < memo[i][j]) ans = memo[i][j];
                 }
@@ -13,3 +14,5 @@ class Solution {
         return ans;
     }
 }
+//n*n
+//n*n
