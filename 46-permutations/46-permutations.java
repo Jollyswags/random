8f98ae13f9f02c1bb932ff6ec24e0cc34661823b
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-06-04 14:11:00 +0530

    Time: 1 ms (95.98%), Space: 42.1 MB (99.59%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,23 +1,22 @@
 class Solution {
-     List<Integer> list = new ArrayList<>();
+       List<Integer> list = new ArrayList<>();
     List<List<Integer>> ansList = new ArrayList<>();
 
     void helper(int[] nums) {
 
-        if (list.size() == nums.length)
-            ansList.add(new ArrayList<>(list));
-
-        else {
-
-            for (int i = 0; i < nums.length; i++) {
-
-                if (list.contains(nums[i]))
+       if(list.size()==nums.length)
+       {
+           ansList.add(new ArrayList<>(list));
+       }
+        else
+        {
+            for(int i=0; i<nums.length; i++)
+            {
+                if(list.contains(nums[i]))
                     continue;
-
                 list.add(nums[i]);
                 helper(nums);
-                list.remove(list.size() - 1);
-
+                list.remove(list.size()-1);
             }
         }
     }
