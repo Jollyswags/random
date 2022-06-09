b0685dee38b622693de00c1fac9e1a5d79b4c51d
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-06-09 21:13:55 +0530

    Time: 57 ms (15.46%), Space: 43.7 MB (22.58%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,18 +1,21 @@
 class Solution {
     public int numTeams(int[] rating) {
-        int res = 0;
-    for (int i = 1; i < rating.length - 1; ++i) {
-        int less[]=new int[2], greater[]=new int[2];
-        for (int j = 0; j < rating.length; ++j) {
-            if (rating[i] < rating[j])
-                ++less[j > i?1:0];
-            if (rating[i] > rating[j])
-                ++greater[j > i ?1 :0];
+        int ans= 0;
+        for(int i= 1; i<rating.length; i++)
+        {
+            int less[]= new int[2];
+            int greater[]= new int[2];
+            for(int j= 0; j< rating.length ;j++)
+            {
+                if(rating[i]<rating[j])
+                    ++less[j>i?1:0];
+                else
+                    if(rating[i]>rating[j])
+                        ++greater[j>i?1:0];
+            }
+            ans+= less[0]*greater[1] + less[1]*greater[0];
         }
-        res += less[0] * greater[1] + greater[0] * less[1];
-    }
-    return res;
+        return ans;
+        
     }
 }
-    //n^2
-    //1
