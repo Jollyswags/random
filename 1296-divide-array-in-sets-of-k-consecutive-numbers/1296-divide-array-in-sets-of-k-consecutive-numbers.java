78890daa1d1c0e996643afbf53780cd629f8672b
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-06-10 21:13:17 +0530

    Time: 152 ms (60.24%), Space: 71.2 MB (63.83%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -1,39 +1,39 @@
 class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
-          Map<Integer, Integer> map = new HashMap<>();
+        int count=0;
+        HashMap<Integer, Integer> map =new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (Integer key : map.keySet()) {
-            if (!map.containsKey(key - 1)) {
-                q.offer(key);
-            }
+        Queue<Integer> q= new LinkedList<>();
+        for(Integer i : map.keySet())
+        {
+            if(!map.containsKey(i-1))
+                q.offer(i);
         }
-        
-        int count = 0;
-        while (!q.isEmpty()) {
+        while(!q.isEmpty())
+        {
             int start = q.poll();
-            for (int i = start; i < start + k; i++) {
+            for(int i= start; i<start + k; i++)
+            {
                 int rest = map.getOrDefault(i, 0);
-                if (rest == 0) {
+                if(rest==0)
                     return false;
-                }
                 count++;
-                map.put(i, rest - 1);
-                if (map.get(i) == 0) {
+                map.put(i, rest-1);
+                if(map.get(i)==0)
+                {
                     map.remove(i);
                 }
-                else {
-                    if (!map.containsKey(i - 1)) {
+                else
+                {
+                    if(!map.containsKey(i-1))
                         q.offer(i);
-                    }    
                 }
             }
-            if (map.containsKey(start + k) && !map.containsKey(start + k - 1)) {
-                q.offer(start + k);
-            }
+            if(map.containsKey(start+k) && !map.containsKey(start+k-1))
+                q.offer(start+k);
+            
         }
         return count == nums.length;
     }
