7529fd94c97ade54c30267653513b53b2c6b401e
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-06-11 14:12:42 +0530

    Time: 7 ms (88.32%), Space: 42.1 MB (96.42%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -3,14 +3,17 @@ class Solution {
          HashMap<Integer, Integer> record = new HashMap<>();
         int count = 0;
         for (int i:nums) {
-            if (k == 0) {
-                if (record.containsKey(i) && record.get(i) == 1) {
+            if(k==0)
+            {
+                if(record.containsKey(i) && record.get(i)==1)
+                {
                     count++;
                 }
-                record.merge(i, 1, Integer::sum);
-            } 
-            else {
-                if (record.containsKey(i + k) && !record.containsKey(i)) {
+                record.merge(i,1,Integer::sum);
+            }
+            else
+            {
+                  if (record.containsKey(i + k) && !record.containsKey(i)) {
                     count++;
                 }
                 if (record.containsKey(i - k) && !record.containsKey(i)) {
