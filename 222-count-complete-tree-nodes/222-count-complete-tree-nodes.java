bd2938ea361ec4c094d515e75f8d7e19e19e6125
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-06-12 21:38:59 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (83.22%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -15,25 +15,8 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int c=0;
-        while(!q.isEmpty())
-        {
-            int s=q.size();
-            for(int i=0;i<s;i++)
-            {
-                TreeNode node=q.poll();
-                if(node==null)
-                    return c;
-                if((node.left!=null && node.right!=null)||(node.left!=null && node.right==null) ||(node.left==null && node.right==null))
-                    c++;
-                if(node.left!=null)
-                    q.add(node.left);
-                if(node.right!=null)
-                    q.add(node.right);
-            }
-        }
-        return c;
+         if (root == null)
+        return 0;
+    return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
