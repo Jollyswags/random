e4ec9654eaf2deefe6a045eb5919ac136e34fa5c
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-06-17 13:56:14 +0530

    Time: 3 ms (12.16%), Space: 44.1 MB (14.09%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,39 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int ans=0;
-    Set<TreeNode> set=new HashSet<>();
+    int ans;
+    Set<TreeNode> covered;
     public int minCameraCover(TreeNode root) {
-        set.add(null);
-        dfs(root,null);
+        ans = 0;
+        covered = new HashSet();
+        covered.add(null);
+
+        dfs(root, null);
         return ans;
     }
-    public void dfs(TreeNode root, TreeNode par)
-    {
-        if(root==null)
-            return;
-        dfs(root.left,root);
-        dfs(root.right,root);
-        if(par==null && !set.contains(root) || !set.contains(root.left) || !set.contains(root.right))
-        {
-            ans++;
-            set.add(root);
-            set.add(par);
-            set.add(root.left);
-            set.add(root.right);
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            dfs(node.left, node);
+            dfs(node.right, node);
+
+            if (par == null && !covered.contains(node) ||
+                    !covered.contains(node.left) ||
+                    !covered.contains(node.right)) {
+                ans++;
+                covered.add(node);
+                covered.add(par);
+                covered.add(node.left);
+                covered.add(node.right);
+            }
         }
     }
 }
