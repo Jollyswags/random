529adaf34bfec364ae58a12cc53a3f739aba99fb
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-07-11 17:26:00 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (95.89%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,39 +14,20 @@
  * }
  */
 class Solution {
-    int maxsum=0;
-    List<Integer>ans=new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> real = new ArrayList<>();
-         List<Integer> left = new ArrayList<>();
-        if(root==null){
-            return real;
+        if (root == null)
+            return new ArrayList<>();
+        List<Integer> rightSide = new ArrayList<>();
+        showRightSideView(rightSide, root, 0);
+        return rightSide;
+    }
+    public void showRightSideView(List<Integer> rightSide, TreeNode node, int depth) {
+        if (node == null)
+            return;
+        if (depth == rightSide.size()) {
+            rightSide.add(node.val);
         }
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        
-        while(!q.isEmpty()){
-            List<Integer> temp = new ArrayList<>();
-            int size = q.size();
-            while(size-->0){
-                if(q.peek().left!=null)  {q.offer(q.peek().left); }
-                if(q.peek().right!=null) {q.offer(q.peek().right);}
-                temp.add(q.poll().val);
-            }
-            
-            ans.add(temp);
-            
-        }
-        for(List<Integer> x: ans)
-            left.add(x.get(0));
-          System.out.println(left);  
-        
-        for(List<Integer> x : ans){
-            real.add(x.get(x.size()-1));
-        }
-        System.out.println(real);
-        return real;
+        showRightSideView(rightSide, node.right, depth + 1);
+        showRightSideView(rightSide, node.left, depth + 1);
     }
 }
