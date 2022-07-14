791c0db5c4c9eb114d09f87adc2da0f27594a057
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-07-14 15:16:48 +0530

    Time: 740 ms (5.11%), Space: 437 MB (5.03%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.java
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -1,29 +1,30 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-    int n=num.length();
-        Stack<Character>s=new Stack<>();
-        for(int i=0;i<n;i++){
-            char ch=num.charAt(i);
-            while(k>0&&s.size()>0&&s.peek()>ch){
-                s.pop();
+         int len = num.length();
+        if(k==0)return num;
+        if(k==len)return "0";
+        Stack<Character> st = new Stack<>();
+        int i=0;
+        while(i<num.length()){
+            while(k>0 && !st.isEmpty() && st.peek()>num.charAt(i)){
+                st.pop();
                 k--;
+                
             }
-            s.push(ch);
+            st.push(num.charAt(i));
+            i++;
         }
-        StringBuilder sb=new StringBuilder();
-        while(s.size()>0){
-            char ch=s.pop();
-            if(ch!='0'&&k>0){
-                k--;
-            }
-            else sb.append(ch);
+        
+        while(k-- > 0){
+            st.pop();
         }
-        sb=sb.reverse();
-        for(int i=0;i<sb.length();i++){
-            if(sb.charAt(i)!='0'){
-                return sb.substring(i);
-            }
+        String smallest = "";
+        while(st.size()!=0){
+            smallest = st.pop()+smallest;
         }
-        return "0";
+        while(smallest.length()>1 && smallest.charAt(0)=='0'){
+            smallest = smallest.substring(1);
+        }
+        return smallest; 
     }
 }
