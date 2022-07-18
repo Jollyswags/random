3e4f467f44dc44ac212884cef860fd7aa362ffe0
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-07-18 22:13:13 +0530

    Time: 3 ms (46.79%), Space: 54.1 MB (41.17%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,13 +1,37 @@
 class Solution {
     int root[];
     int rank[];
-    public int findCircleNum(int[][] isConnected) 
+    public int find(int x)
     {
+        if(root[x]==x)
+            return x;
+        return root[x]=find(root[x]);
+    }
+    public void union(int x, int y)
+    {
+        int rootX= find(x);
+        int rootY=find(y);
+        if(rank[rootX]> rank[rootY])
+        {
+            root[rootY]=root[rootX];
+        }
+        else
+            if(rank[rootY]<rank[rootX])
+            {
+                root[rootX]=root[rootY];
+            }
+        else
+        {
+            root[rootY]=rootX;
+            rank[rootX]++;
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int l =isConnected.length;
         int count=0;
-        int l=isConnected.length;
         root=new int[l];
         rank=new int[l];
-        int i,j;
+         int i,j;
         for(i=0;i<l;i++)
         {
             root[i]=i;
@@ -23,33 +47,7 @@ class Solution {
                 count++;
         }
         return count;
-    }
-    public int find(int x)
-    {
-        if(root[x]==x)
-            return x;
-        return root[x]=find(root[x]);
-    }
-    public void union(int x, int y)
-    {
-        int rootX=find(x);
-        int rootY=find(y);
-        if(rank[rootX]>rank[rootY])
-        {
-            root[rootY]=root[rootX];
-            
-        }
-        else
-            if(rank[rootX]<rank[rootY])
-            {
-                root[rootX]=root[rootY];
-            }
-        else
-        {
-            root[rootY]=rootX;
-            rank[rootX]++;
-        }
+        
+        
     }
 }
-// TC=O(N)
-//SC=O(N)
