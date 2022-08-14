b28629bfa79c5a74bef724ae9f16cc4ad4a33498
Jolly <87942676+Jollyswags@users.noreply.github.com>
2022-08-14 16:59:42 +0530

    Time: 38 ms (29.15%), Space: 43.9 MB (81.10%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.java
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -1,82 +1,165 @@
 class Solution {
+    
+    ArrayList<ArrayList<Integer>> adj =  new ArrayList<>();
+    List<List<Integer>> paths = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> parent = new ArrayList<>();
+    
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        wordList.add(0, beginWord);
+        int w = wordList.size();
+        int i, j;
         
-        List<List<Integer>> g = buildGraph(wordList);
+        for(i=0;i<w+1;i++){
+            adj.add(new ArrayList<>());
+        }
         
-        List<List<String>> res = new ArrayList<>();
-        int min = Integer.MAX_VALUE;
-        int target = wordList.indexOf(endWord);
-        int source = wordList.indexOf(beginWord);
-        Queue<Pair> q = new ArrayDeque<>();
-        q.add(new Pair<Integer, List<String>>(source, new ArrayList<>()));
-        int[] visited = new int[wordList.size()];
+        for(i=0;i<w;i++){
+            if(checkSingleDiff(beginWord, wordList.get(i)) == 1){
+                addEdge(0, i+1);
+            }
+        }
         
-        while (q.size() > 0) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                Pair<Integer, List<String>> node = q.poll();
-                Integer index = node.getKey();
-                visited[index] = 1;
-                String word = wordList.get(index);
-                List<String> path = node.getValue();
-                path.add(word);
-                if (path.size() > min) continue;
-                if (index == target) {
-                    if (path.size() < min) {
-                        min = path.size();
-                        res.clear();
-                        res.add(path);
-                    } else if (path.size() == min) {
-                        res.add(path);
-                    }
-                } else {
-                    for (Integer nei: g.get(index)) {
-                        String neiWord = wordList.get(nei);
-                        if (visited[nei] == 0) {
-                            q.add(new Pair<>(nei, new ArrayList<>(path)));
-                        }
-                    }
+        for(i=0;i<w;i++){
+            for(j=i+1;j<w;j++){
+                if(checkSingleDiff(wordList.get(i), wordList.get(j)) == 1){
+                    addEdge(i+1, j+1);
                 }
             }
         }
         
-        return res;
+        // System.out.println("ADJ is");
+        // for(i=0;i<adj.size();i++){
+        //     System.out.print(i + " -> ");
+        //     ArrayList<Integer> temp = adj.get(i);
+        //     for(j=0;j<temp.size();j++){
+        //         System.out.print(temp.get(j) + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        int beginWordInd = 0;
+        int endWordInd = -1;
+        for(i=0;i<w;i++){
+            if(wordList.get(i).equals(endWord)){
+                endWordInd = i+1;
+                break;
+            }
+        }
+        
+        List<List<String>> ans = new ArrayList<>();
+        if(endWordInd != -1){ 
+            ans = getShortestSequences(beginWordInd, endWordInd, w+1, wordList, beginWord);
+        }
+        
+        return ans;
     }
     
-    boolean intersects(String a, String b) {
-        int i = 0;
-        int count = 0;
-        while (i < a.length()) {
-            if (a.charAt(i) != b.charAt(i)) {
-                count++;
-            }
-            if (count > 1) {
-                return false;
-            }
-            i++;
-        }
-        return count == 1;
-     }
-    
-     List<List<Integer>> buildGraph(List<String> wordList) {
-        int n = wordList.size();
-        List<List<Integer>> g = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            g.add(new ArrayList<>());
+    private List<List<String>> getShortestSequences(int src, int dest, int vertices, List<String> wordList, String beginWord){
+        ArrayList<Integer> path = new ArrayList<>();
+        
+        int i, j;
+        for(i=0;i<vertices;i++){
+            parent.add(new ArrayList<>());
         }
         
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (intersects(wordList.get(i), wordList.get(j))) {
-                    g.get(i).add(j);
-                    g.get(j).add(i);
+        BFS(vertices, src, wordList);
+        
+        getShortestSequencesUtil(path, vertices, dest);
+        
+        // System.out.println("Size of paths is " + paths.size());
+        for(i=0;i<paths.size();i++){
+            Collections.reverse(paths.get(i));
+            paths.set(i, paths.get(i));
+        }
+        
+        List<List<String>> ans = new ArrayList<>();
+        
+        for(i=0;i<paths.size();i++){
+            List<Integer> p = paths.get(i);
+            ArrayList<String> pp = new ArrayList<>();
+            for(j=0;j<p.size();j++){
+                if(p.get(j) == 0){
+                    pp.add(beginWord);
+                }
+                else{
+                    pp.add(wordList.get(p.get(j) - 1));   
+                }
+            }
+            ans.add(pp);
+        }
+        
+        return ans;
+    }
+    
+    private void getShortestSequencesUtil(ArrayList<Integer> path, int vertices, int dest){
+        if(dest == -1){
+            paths.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int p : parent.get(dest)){
+            path.add(dest);
+            getShortestSequencesUtil(path, vertices, p);
+            path.remove(path.size() - 1);
+        }
+    }
+    
+    private void BFS(int vertices, int src, List<String> wordList){
+        int shortestDist[] = new int[vertices];
+        Arrays.fill(shortestDist, Integer.MAX_VALUE);
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        parent.get(src).clear();
+        parent.get(src).add(-1);
+        shortestDist[src] = 0;
+        
+        while(q.isEmpty() == false){
+            int u = q.poll();
+            for(int v : adj.get(u)){
+                if(shortestDist[u] + 1 < shortestDist[v]){
+                    shortestDist[v] = shortestDist[u] + 1;
+                    q.offer(v);
+                    parent.get(v).clear();
+                    parent.get(v).add(u);
+                }
+                else if(shortestDist[u] + 1 == shortestDist[v]){
+                    parent.get(v).add(u);
                 }
             }
         }
-        return g;
-     }
+        
+        // System.out.println("Parent is ");
+        // for(int i=0;i<parent.size();i++){
+        //     ArrayList<Integer> p = parent.get(i);
+        //     System.out.print(i + " -> ");
+        //     for(int j=0;j<p.size();j++){
+        //         System.out.print(p.get(j) + " ");    
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+        
+        // System.out.println("Shortest distance is ");
+        // for(int i=0;i<shortestDist.length;i++){
+        //     System.out.print(shortestDist[i] + " ");
+        // }
+        // System.out.println();
+    }
+    
+    private void addEdge(int u, int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+    
+    private int checkSingleDiff(String a, String b){
+        int i;
+        int count = 0;
+        for(i=0;i<a.length();i++){
+            if(a.charAt(i) != b.charAt(i)){
+                count++;
+            }
+        }
+        
+        return count;
+    }
 }
-//Bidirectional bfs
-//O(NN)
-//O(N)
